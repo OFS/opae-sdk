08562b17888b349db3159b119068dc176a5fe0d4
Lacasse, RobertX <robertx.lacasse@intel.com>
2017-10-13 01:35:37 +0800

    fpgainfo: Parse feature revision for errors
    This update resolved the stack-dump problem with fpgainfo errors on
    revision 1 targets.

--- tools/fpgainfo/fpgaerr.py
+++ tools/fpgainfo/fpgaerr.py
@@ -1,28 +1,28 @@
-# Copyright(c) 2017, Intel Corporation
-#
-# Redistribution  and  use  in source  and  binary  forms,  with  or  without
-# modification, are permitted provided that the following conditions are met:
-#
-# * Redistributions of  source code  must retain the  above copyright notice,
-#  this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#   and/or other materials provided with the distribution.
-# * Neither the name  of Intel Corporation  nor the names of its contributors
-#   may be used to  endorse or promote  products derived  from this  software
-#   without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
-# IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
-# LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
-# CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
-# SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
-# INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
-# CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
+
+-#
+-# Redistribution  and  use  in source  and  binary  forms,  with  or  without
+-# modification, are permitted provided that the following conditions are met:
+-#
+-# * Redistributions of  source code  must retain the  above copyright notice,
+-#  this list of conditions and the following disclaimer.
+-# * Redistributions in binary form must reproduce the above copyright notice,
+-#  this list of conditions and the following disclaimer in the documentation
+-#   and/or other materials provided with the distribution.
+-# * Neither the name  of Intel Corporation  nor the names of its contributors
+-#   may be used to  endorse or promote  products derived  from this  software
+-#   without specific prior written permission.
+-#
+-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+-# IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+-# ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
+-# LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
+-# CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
+-# SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
+-# INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
+-# CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+-# POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import os.path
@@ -264,8 +264,6 @@ class errors_command(fpga_command):
         if os.path.isfile(revpath):
             with open(revpath, 'r') as fd:
                 value = fd.read().strip()
-            print "GBS Revision:  " + value
-            print "(" + revpath + ")"
             if value != 0:
                 return value
             else:
@@ -306,35 +304,39 @@ class errors_command(fpga_command):
         elif args.which == 'port':
             errpath = os.path.join(PORT_ERRORS, 'errors')
         elif args.which == 'first_error':
-
             errpath = os.path.join(PORT_ERRORS, 'first_error')
         else:
             errpath = os.path.join(FME_ERRORS, '{}_errors'.format(args.which))
         errpath = errpath.format(socket_id=args.socket_id)
         print errpath
-        with open(errpath, 'r') as fd:
-            value = fd.read().strip()
-        # TODO : interpret the value
-
+        try:
+            with open(errpath, 'r') as fd:
+                value = fd.read().strip()
+            # TODO : interpret the value
+        except IOError:
+            print "unsupported on this revision"
+            return
         rev = errors_command.get_revision(args)
         if rev == 0:
             err_class = err_classes_rev0.get(args.which, error_mask)
         else:
             err_class = err_classes_rev1.get(args.which, error_mask)
-
         errs = err_class(int(value, 16))
-
         print errs
         if errs.value and args.clear:
-            clear_path = os.path.join(os.path.dirname(errpath), 'clear')
-            with open(clear_path, 'w') as fd:
-                fd.write(value)
-            with open(errpath, 'r') as fd:
-                value = fd.read().strip()
-            radix = 16 if value.startswith('0x') else 10
-            errs = err_class(int(value, radix))
-            print errs
-
+            try:
+                clear_path = os.path.join(
+                    os.path.dirname(errpath), 'clear')
+            except IOError:
+                print "unsupported on this device"
+                return
+        with open(clear_path, 'w') as fd:
+            fd.write(value)
+        with open(errpath, 'r') as fd:
+            value = fd.read().strip()
+        radix = 16 if value.startswith('0x') else 10
+        errs = err_class(int(value, radix))
+        print errs
 
 if __name__ == "__main__":
     import argparse
