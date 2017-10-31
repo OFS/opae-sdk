b96051406f1e016d21bd17a835185ec57949ab5c
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-10-31 15:37:38 -0700

    ASE: Python Scripts fix in ipc_clean.py

--- ase/scripts/ipc_clean.py
+++ ase/scripts/ipc_clean.py
@@ -99,8 +99,8 @@ for rfile in ready_list:
 
 
 # Kill all ase_simv processes started by by $USER #
-cleanme_input = raw_input('Type 'y' to clean up all zombie ase_simv '
-                          'processes : ')
+cleanme_input = raw_input("Type 'y' to clean up all zombie ase_simv "
+                          "processes : ")
 if cleanme_input == "y":
     print("Going ahead with cleaning up ASE processes opened by ", USER)
     os.system("pgrep ase_simv -u " + USER + " | xargs kill -9 ")
