605ba613d1cfd3b756cdafd73bc76cd89d9e6d72
Michael Adler <Michael.Adler@intel.com>
2017-11-06 16:34:34 -0500

    ASE script cleanup

--- ase/scripts/generate_ase_environment.py
+++ ase/scripts/generate_ase_environment.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
-## Copyright(c) 2013-2017, Intel Corporation
-##
-## Redistribution  and  use  in source  and  binary  forms,  with  or  without
-## modification, are permitted provided that the following conditions are met:
-##
-## * Redistributions of  source code  must retain the  above copyright notice,
-##   this list of conditions and the following disclaimer.
-## * Redistributions in binary form must reproduce the above copyright notice,
-##   this list of conditions and the following disclaimer in the documentation
-##   and/or other materials provided with the distribution.
-## * Neither the name  of Intel Corporation  nor the names of its contributors
-##   may be used to  endorse or promote  products derived  from this  software
-##   without specific prior written permission.
-##
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-## AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
-## IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-## ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
-## LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
-## CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
-## SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
-## INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
-## CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
-## ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
-## POSSIBILITY OF SUCH DAMAGE.
+# Copyright(c) 2013-2017, Intel Corporation
+#
+# Redistribution  and  use  in source  and  binary  forms,  with  or  without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of  source code  must retain the  above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name  of Intel Corporation  nor the names of its contributors
+#   may be used to  endorse or promote  products derived  from this  software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+# IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
+# LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
+# CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
+# SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
+# INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
+# CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 #######################################################################
 # generate_ase_environment.py : Generate AFU paths, include directories as a
@@ -51,7 +51,9 @@
 from __future__ import print_function
 import argparse
 import ase_functions
-import os, re, sys
+import os
+import re
+import sys
 from collections import defaultdict
 from fnmatch import fnmatch
 import json
@@ -64,23 +66,20 @@ if sys.version_info < (2, 7):
 else:
     import subprocess
 
-### Supported file extensions
+# Supported file extensions
 # USERs may modify this if needed
-VLOG_EXTENSIONS = [ ".svh", ".sv", ".vs", ".v" ]
-VHD_EXTENSIONS = [ ".vhd", ".vhdl" ]
+VLOG_EXTENSIONS = [".svh", ".sv", ".vs", ".v"]
+VHD_EXTENSIONS = [".vhd", ".vhdl"]
 
 VHDL_FILE_LIST = os.environ['PWD'] + "/vhdl_files.list"
 VLOG_FILE_LIST = os.environ['PWD'] + "/vlog_files.list"
-TOOL_BRAND     = "VCS"
+TOOL_BRAND = "VCS"
 
 # Forbidden characters
-SPECIAL_CHARS='\[]~!@#$%^&*(){}:;+$\''
+SPECIAL_CHARS = '\[]~!@#$%^&*(){}:;+$\''
 
-##########################################################
-###                                                    ###
-###        DO NOT MODIFY BELOW THIS COMMENT BLOCK      ###
-###                                                    ###
-##########################################################
+
+#        DO NOT MODIFY BELOW THIS COMMENT BLOCK     #
 # Global variables
 arg_list = []
 tolowarg_list = []
@@ -94,11 +93,16 @@ def errorExit(msg):
 
 def remove_dups(filepath, exclude=None):
     import hashlib
-    include = lambda f : exclude is None or not fnmatch(f, exclude)
+
+    def include(f):
+        if exclude is None or not fnmatch(f, exclude):
+            return True
+        else:
+            return False
     files = []
     hashes = dict()
     with open(filepath, 'r') as fd:
-        files = filter(lambda l : l.strip() != '', fd.readlines())
+        files = filter(lambda l: l.strip() != '', fd.readlines())
     files = [f.strip() for f in files]
     for f in files:
         with open(f, 'r') as fd:
@@ -118,9 +122,9 @@ def remove_dups(filepath, exclude=None):
 
 
 
-#################### Run command and get string output #######################
+# Run command and get string output #
 def commands_getoutput(cmd):
-    if sys.version_info < (2,7):
+    if sys.version_info < (2, 7):
         return commands.getoutput(cmd)
     else:
         byte_out = subprocess.check_output(cmd.split())
@@ -128,46 +132,29 @@ def commands_getoutput(cmd):
         return str_out
 
 def commands_list_getoutput(cmd):
-    if sys.version_info < (2,7):
+    if sys.version_info < (2, 7):
         return commands.getoutput(' '.join(cmd))
     else:
         byte_out = subprocess.check_output(cmd)
         str_out = byte_out.decode("utf-8")
         return str_out
 
-############################# Print Help #####################################
-def show_help():
-    ase_functions.begin_red_fontcolor()
-    print("INCORRECT command, CORRECT (required) usage is:")
-    print("python generate_ase_environment.py <dir 1> [dir 2] ... [dir n] [-t <VCS|QUESTA>] [-p <FPGA_PLATFORM_INTG_XEON|FPGA_PLATFORM_DISCRETE>]")
-    print("")
-    print("Required switches => ")
-    print("     <dir1>           => Atleast one sources directory is required")
-    print("     [dir2]...[dir n] => Other optional directories with sources")
-    print("")
-    print("Optional switches => ")
-    print("     -h,--help        => Show this help message")
-    print("     -t,--tool        => Enter tool type as 'VCS' or 'QUESTA'")
-    print("     -p,--plat        => Enter platform type as 'intg_xeon' or 'discrete'")
-    print("")
-    ase_functions.end_red_fontcolor()
 
-########################### Has duplicates ###################################
+# Has duplicates #
 def has_duplicates(word_dict):
-    dups = filter(lambda (k,v) : len(v) > 1, word_dict.items())
+    dups = filter(lambda (k, v): len(v) > 1, word_dict.items())
     if dups:
         print("Duplicates found -")
-        for k,v in dups:
+        for k, v in dups:
             print(k)
             print('\n'.join(['\t{}'.format(l) for l in v]))
         return True
     return False
 
 
-#############################################################################
-##    Case #1: Construct build from a list of files                        ##
-#############################################################################
-
+#
+# Case #1: Construct build from a list of files
+#
 def config_sources(fd, filelist):
     # Get all the sources.  rtl_src_config will emit all relevant source
     # files, one per line.
@@ -267,31 +254,27 @@ def config_sources(fd, filelist):
     return json_file
 
 
-#############################################################################
-##    Case #2: Construct build automatically by looking for RTL sources    ##
-#############################################################################
+#
+# Case #2: Construct build automatically by looking for RTL sources
+#
 def auto_find_sources(fd):
-    #######################################################################
     # Prepare list of candidate directories
     print("Valid directories supplied => ")
     valid_dirlist = filter(lambda p : os.path.exists(p), args.dirlist)
     str_dirlist = " ".join(valid_dirlist)
     if len(valid_dirlist) == 0:
-        ase_functions.begin_red_fontcolor()
-        print("No Valid source directories were specified ... please re-run script with legal directory name")
-        show_help()
-        ase_functions.end_red_fontcolor()
-        sys.exit(1)
+        # This line should never be reached since the directory list was
+        # already checked after argument parsing.
+        errorExit("No source directories specifield")
 
-    ########################################################
     # Check if VHDL files exist, populate if any
-    ########################################################
     print("")
     print("Finding VHDL files ... ")
     str = ""
     vhdl_filepaths = ""
     for extn in VHD_EXTENSIONS:
-        str = str + commands_getoutput("find -L " + str_dirlist + " -type f -name *" + extn)
+        str = str + commands_getoutput("find -L " + str_dirlist + ' -type f -name '
+                                       ' *' + extn)
         if len(str) != 0:
             str = str + "\n"
     if len(str.strip()) != 0:
@@ -302,9 +285,7 @@ def auto_find_sources(fd):
     else:
         print("No VHDL files were found !")
 
-    ########################################################
     # Check if V/SV files exist, populate if any
-    ########################################################
     print("")
     print("Finding {System}Verilog files ... ")
     str = ""
@@ -330,9 +311,7 @@ def auto_find_sources(fd):
 
     vlog_filepaths = remove_dups(VLOG_FILE_LIST, args.exclude)
 
-    ########################################################
     # Recursively find and add directory locations for VH
-    ########################################################
     print("")
     print("Finding include directories ... ")
     str = commands_getoutput("find -L " + str_dirlist + " -type d")
@@ -341,9 +320,7 @@ def auto_find_sources(fd):
         print("DUT_INCDIR = " + str)
         fd.write("DUT_INCDIR = " + str + "\n\n")
 
-    #############################################
     # Module repetition check
-    #############################################
     vhdl_filepaths = vhdl_filepaths.replace("\n", " ").split()
     vlog_filepaths = vlog_filepaths.replace("\n", " ").split()
 
@@ -370,9 +347,7 @@ def auto_find_sources(fd):
         print("Please remove them manually as RTL compilation is expected to FAIL !")
         ase_functions.end_red_fontcolor()
 
-    ############################################################
     # Search for a JSON file describing the top-level interface
-    ############################################################
     json_file = None
     str = commands_getoutput("find -L " + str_dirlist + " -type f -name *.json")
     if (len(str)):
@@ -391,16 +366,15 @@ def auto_find_sources(fd):
             except:
                 None
 
-    #############################################
     # Print auto-find instructions
-    #############################################
     print("")
     ase_functions.begin_green_fontcolor()
     print("NOTES TO USER => ")
     print("* This script assumes File Extensions: ")
     print("  * VHDL : .vhd")
     print("  * V/SV : .sv .vs .v")
-    print("  * If you use arbitrary extensions, please edit this script to reflect them, and re-run the script")
+    print('  * If you use arbitrary extensions, please edit this script to '
+          'reflect them, and re-run the script')
     print("* See ase_sources.mk and check for correctness")
     print("* See if DUT_INCDIR has all the locations mentioned")
     print("  * If a directory is missing, append it separated by '+' symbol")
@@ -410,9 +384,7 @@ def auto_find_sources(fd):
     return json_file
 
 
-#############################################################################
-##    AFU / Platform interface configuration                               ##
-#############################################################################
+# AFU / Platform interface configuration
 def gen_afu_platform_ifc(json_file):
     cfg = "afu_platform_config --sim --tgt=rtl".split(" ")
 
@@ -434,9 +406,6 @@ def gen_afu_platform_ifc(json_file):
         errorExit("Aborting ASE configuration")
 
 
-#############################################################################
-##                        Script begins here                               ##
-#############################################################################
 print("#################################################################")
 print("#                                                               #")
 print("#             OPAE Intel(R) Xeon(R) + FPGA Library              #")
@@ -463,8 +432,8 @@ parser.add_argument('-s', '--sources',
                             parsed by rtl_src_config.""")
 parser.add_argument('-t', '--tool', choices=['VCS', 'QUESTA'], default='VCS',
                     help='simulator tool to use, default is VCS')
-parser.add_argument('-p', '--plat', choices=['intg_xeon', 'discrete'], default='intg_xeon',
-                    help='FPGA Platform to simulate')
+parser.add_argument('-p', '--plat', choices=['intg_xeon', 'discrete'],
+                    default='intg_xeon', help='FPGA Platform to simulate')
 parser.add_argument('-x', '--exclude', default=None,
                     help='file name pattern to exclude (only in auto-discovery mode)')
 
@@ -480,13 +449,11 @@ if len(args.dirlist) and args.sources:
 tool_type = args.tool
 TOOL_BRAND = args.tool
 PLAT_TYPE = {'intg_xeon': 'FPGA_PLATFORM_INTG_XEON',
-             'discrete' : 'FPGA_PLATFORM_DISCRETE'}.get(args.plat)
+             'discrete': 'FPGA_PLATFORM_DISCRETE'}.get(args.plat)
 print("\nTool Brand: ", TOOL_BRAND)
 print("Platform Type: ", PLAT_TYPE)
 
-########################################################
-### Write Makefile snippet ###
-########################################################
+# Write Makefile snippet #
 fd = open("ase_sources.mk", "w")
 # Print Information in ase_sources.mk
 fd.write("##############################################################\n")
@@ -497,23 +464,20 @@ fd.write("#                                                            #\n")
 fd.write("##############################################################\n")
 fd.write("\n")
 
-#############################################
+
 # Update SIMULATOR
-#############################################
 fd.write("SIMULATOR ?= ")
 fd.write(TOOL_BRAND)
 fd.write("\n\n")
 
-#############################################
+
 # Update ASE_PLATFORM
-#############################################
 fd.write("ASE_PLATFORM ?= ")
 fd.write(PLAT_TYPE)
 fd.write("\n\n")
 
-#############################################
+
 # Configure RTL sources
-#############################################
 if (args.sources):
     # Sources are specified explicitly in a file containing a list of sources
     json_file = config_sources(fd, args.sources)
@@ -527,16 +491,16 @@ gen_afu_platform_ifc(json_file)
 
 fd.close()
 
-#############################################
+
 # Write tool specific scripts
-#############################################
 
 ## Scripts for multiple tools are written since the default tool
 ## can be selected at build time.
 
 # Write Synopsys Setup file & TCL script
 open("synopsys_sim.setup", "w").write("WORK > DEFAULT\nDEFAULT : ./work\n")
-open("vcs_run.tcl", "w").write("dump -depth 0 \ndump -aggregates -add / \nrun \nquit\n")
+open("vcs_run.tcl", "w").write('dump -depth 0 \ndump -aggregates -add /'
+                               '\nrun \nquit\n')
 
 # Generate .DO file
 open("vsim_run.tcl", "w").write("add wave -r /* \nrun -all\n")
