184787293ef2c2f91d9c68998df56e8cd2320e33
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-11-21 14:19:03 -0800

    Add travis CI script

---
+++ scripts/test-gtapi-mock-driver.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+mkdir mybuild_gtapi_mock_drv
+pushd mybuild_gtapi_mock_drv
+
+trap "popd" EXIT
+
+cmake .. -DBUILD_TESTS=ON
+make
+
+make test
+
+echo "test-gtapi-mock-drv build PASSED"
