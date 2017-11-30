60a470d7b8fc0d6ebc270736c48f3fabacfdf060
Buie, Jonathan <jon.buie@intel.com>
2017-11-30 12:39:11 -0800

    sync fpga_dma_test source code (#244)
    sync the fpga_dma_test source code for use with the fpgabist.
    Code copied from the intel-fpga-afu repository -> release/0.13 branch
    
    https://github.intel.com/OPAE/intel-fpga-afu/tree/release/0.13/dma_afu/sw

--- tools/fpgabist/dma/fpga_dma_internal.h
+++ tools/fpgabist/dma/fpga_dma_internal.h
@@ -38,8 +38,13 @@
 #define IS_ALIGNED_DWORD(addr) (addr%4==0)
 #define IS_ALIGNED_QWORD(addr) (addr%8==0)
 
-#define FPGA_DMA_UUID_H 0xd79c094c7cf94cc1
-#define FPGA_DMA_UUID_L 0x94eb7d79c7c01ca3
+#define FPGA_DMA_UUID_H 0xef82def7f6ec40fc
+#define FPGA_DMA_UUID_L 0xa9149a35bace01ea
+#define FPGA_DMA_WF_MAGIC_NO          0x5772745F53796E63ULL
+#define FPGA_DMA_HOST_MASK            0x2000000000000
+#define FPGA_DMA_WF_HOST_MASK         0x3000000000000
+#define FPGA_DMA_WF_ROM_MAGIC_NO_MASK 0x1000000000000
+
 
 #define AFU_DFH_REG 0x0
 #define AFU_DFH_NEXT_OFFSET 16
@@ -77,61 +82,155 @@
 #define FPGA_DMA_BUF_ALIGN_SIZE FPGA_DMA_BUF_SIZE
 // Convenience macros
 #ifdef FPGA_DMA_DEBUG
-  #define debug_print(fmt, ...) \
-          do { if (FPGA_DMA_DEBUG) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
+	#define debug_print(fmt, ...) \
+					do { if (FPGA_DMA_DEBUG) fprintf(stderr, fmt, ##__VA_ARGS__); } while (0)
 #else
-  #define debug_print(...)
+	#define debug_print(...)
 #endif
 
-#define MAX_BUF 4
+#define FPGA_DMA_MAX_BUF 8
 
 typedef union {
-   uint64_t reg;
-   struct {
-      uint64_t feature_type:4;
-      uint64_t reserved_8:8;
-      uint64_t afu_minor:4;
-      uint64_t reserved_7:7;
-      uint64_t end_dfh:1;
-      uint64_t next_dfh:24;
-      uint64_t afu_major:4;
-      uint64_t feature_id:12;
-   } bits;
+	uint64_t reg;
+	struct {
+		uint64_t feature_type:4;
+		uint64_t reserved_8:8;
+		uint64_t afu_minor:4;
+		uint64_t reserved_7:7;
+		uint64_t end_dfh:1;
+		uint64_t next_dfh:24;
+		uint64_t afu_major:4;
+		uint64_t feature_id:12;
+	} bits;
 } dfh_reg_t;
 
-typedef struct __attribute__((__packed__))
-{
-  //0x0
-  uint32_t rd_address;
-  //0x4
-  uint32_t wr_address;
-  //0x8
-  uint32_t len;
-  //0xC
-  uint16_t seq_num;
-  uint8_t rd_burst_count;
-  uint8_t wr_burst_count;
-  //0x10
-  uint16_t rd_stride;
-  uint16_t wr_stride;
-  //0x14
-  uint32_t rd_address_ext;
-  //0x18
-  uint32_t wr_address_ext;
-  //0x1c
-  uint32_t control;
-} msgdma_ext_descriptor_t;
-
-struct _dma_handle_t
-{
-   fpga_handle fpga_h;
-   uint32_t mmio_num;
-   uint64_t mmio_offset;
-   uint64_t dma_base;
-   uint64_t dma_offset;
-   uint64_t *dma_buf_ptr[MAX_BUF];
-   uint64_t dma_buf_wsid[MAX_BUF];
-   uint64_t dma_buf_iova[MAX_BUF];
+struct _dma_handle_t {
+	fpga_handle fpga_h;
+	uint32_t mmio_num;
+	uint64_t mmio_offset;
+	uint64_t dma_base;
+	uint64_t dma_offset;
+	uint64_t dma_csr_base;
+	uint64_t dma_desc_base;
+	uint64_t dma_ase_cntl_base;
+	uint64_t dma_ase_data_base;
+	// Interrupt event handle
+	fpga_event_handle eh;
+	// magic number buffer
+	volatile uint64_t *magic_buf;
+	uint64_t magic_iova;
+	uint64_t magic_wsid;
+	uint64_t *dma_buf_ptr[FPGA_DMA_MAX_BUF];
+	uint64_t dma_buf_wsid[FPGA_DMA_MAX_BUF];
+	uint64_t dma_buf_iova[FPGA_DMA_MAX_BUF];
 };
+
+typedef union {
+	uint32_t reg;
+	struct {
+		uint32_t tx_channel:8;
+		uint32_t generate_sop:1;
+		uint32_t generate_eop:1;
+		uint32_t park_reads:1;
+		uint32_t park_writes:1;
+		uint32_t end_on_eop:1;
+		uint32_t reserved_1:1;
+		uint32_t transfer_irq_en:1;
+		uint32_t early_term_irq_en:1;
+		uint32_t trans_error_irq_en:8;
+		uint32_t early_done_en:1;
+		uint32_t reserved_2:6;
+		uint32_t go:1;
+	};
+} msgdma_desc_ctrl_t;
+
+typedef struct __attribute__((__packed__)) {
+	//0x0
+	uint32_t rd_address;
+	//0x4
+	uint32_t wr_address;
+	//0x8
+	uint32_t len;
+	//0xC
+	uint16_t seq_num;
+	uint8_t rd_burst_count;
+	uint8_t wr_burst_count;
+	//0x10
+	uint16_t rd_stride;
+	uint16_t wr_stride;
+	//0x14
+	uint32_t rd_address_ext;
+	//0x18
+	uint32_t wr_address_ext;
+	//0x1c
+	msgdma_desc_ctrl_t control;
+} msgdma_ext_desc_t;
+
+typedef union {
+	uint32_t reg;
+	struct {
+		uint32_t busy:1;
+		uint32_t desc_buf_empty:1;
+		uint32_t desc_buf_full:1;
+		uint32_t rsp_buf_empty:1;
+		uint32_t rsp_buf_full:1;
+		uint32_t stopped:1;
+		uint32_t resetting:1;
+		uint32_t stopped_on_errror:1;
+		uint32_t stopped_on_early_term:1;
+		uint32_t irq:1;
+		uint32_t reserved:22;
+	} st;
+} msgdma_status_t;
+
+typedef union {
+	uint32_t reg;
+	struct {
+		uint32_t stop_dispatcher:1;
+		uint32_t reset_dispatcher:1;
+		uint32_t stop_on_error:1;
+		uint32_t stopped_on_early_term:1;
+		uint32_t global_intr_en_mask:1;
+		uint32_t stop_descriptors:1;
+		uint32_t rsvd:22;
+	} ct;
+} msgdma_ctrl_t;
+
+typedef union {
+	uint32_t reg;
+	struct {
+		uint32_t rd_fill_level:16;
+		uint32_t wr_fill_level:16;
+	} fl;
+} msgdma_fill_level_t;
+
+typedef union {
+	uint32_t reg;
+	struct {
+		uint32_t rsp_fill_level:16;
+		uint32_t rsvd:16;
+	} rsp;
+} msgdma_rsp_level_t;
+
+typedef union {
+	uint32_t reg;
+	struct {
+		uint32_t rd_seq_num:16;
+		uint32_t wr_seq_num:16;
+	} seq;
+} msgdma_seq_num_t;
+
+typedef struct __attribute__((__packed__)) {
+	// 0x0
+	msgdma_status_t status;
+	// 0x4
+	msgdma_ctrl_t ctrl;
+	// 0x8
+	msgdma_fill_level_t fill_level;
+	// 0xc
+	msgdma_rsp_level_t rsp;
+	// 0x10
+	msgdma_seq_num_t seq_num;
+} msgdma_csr_t;
 
 #endif // __FPGA_DMA_INT_H__
