6d808aef6147a8457a4123af08d1f31711c052dd
Rodrigo Rojo <rodrigo.rojo@me.com>
2017-11-30 10:36:20 -0800

    c++: updates to c++ API and add unit tests (#52)
    * c++: Add libopae++ subdirectory to build
    
    * Add cmake option, `BUILD_LIBOPAE_CXX` option and default it to OFF
    * Add cmake directive to add libopae++ subdirectory if the option is ON
    
    * c++: Add second constructor to `properties`
    
    Add a constructor to `properties` that takes in the object type.
    Fix line endings (to unix)
    
    * c++: add unit tests
    
    * c++: Fix sample build to use CMAKE Threads var.
    
    * c++: fix typo, line endings, tab chars

---
+++ libopae++/tests/unit/gtEnumerate.cpp
@@ -0,0 +1,25 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#include "opaec++/token.h"
+#include "gtest/gtest.h"
+using namespace opae::fpga::types;
+
+
+/**
+ * @test cxx_enum_01
+ * Given an environment with at least one accelerator<br>
+ * When I call accelerator::enumerate with no filters<br>
+ * Then I get at least one accelerator object in the return list<br>
+ * And no exceptions are thrown
+ */
+TEST(CxxEnum, enum_01){
+    auto tokens = token::enumerate({});
+    EXPECT_TRUE(tokens.size() > 0);
+    ASSERT_NO_THROW(tokens.clear());
+}
