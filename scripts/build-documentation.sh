127fbd2eed59fa2bce0fe66edb19ef7a003e0ade
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-08 15:02:12 -0800

    Add script to travis

--- scripts/build-documentation.sh
+++ scripts/build-documentation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 pip install -r doc/sphinx/requirements.txt
+
+mkdir mybuild_docs
+pushd mybuild_docs
+
+trap "popd" EXIT
+
+cmake ..
+make sphinx_html
+
+echo "build Sphinx documentation FINISHED"
