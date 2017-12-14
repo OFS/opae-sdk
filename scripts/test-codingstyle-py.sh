f27bcd94a9eeb068a8609bf081061af61cbddd1b
Rodrigo Rojo <rodrigo.rojo@intel.com>
2017-12-14 14:23:43 -0800

    Fix test-codingstyle-py.sh
    Fix test-codingstyle-py.sh to prepend `./` to list of changed files

--- scripts/test-codingstyle-py.sh
+++ scripts/test-codingstyle-py.sh
@@ -11,7 +11,7 @@ FILES+=$(grep -rl "^#./usr/bin.*python" ./* | grep -v cpplint.py | grep -vE "^\.
 if [ "$TRAVIS_COMMIT_RANGE" != "" ]; then
     CHANGED_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
     printf "Looking at changed files:\n${CHANGED_FILES}"
-    FILES=$(comm -12 <(sort <(for f in $FILES; do printf "$f\n"; done)) <(sort <(for f in $CHANGED_FILES; do printf "$f\n"; done)))
+    FILES=$(comm -12 <(sort <(for f in $FILES; do printf "$f\n"; done)) <(sort <(for f in $CHANGED_FILES; do printf "./$f\n"; done)))
 fi
 
 if [ "$1" == "-v" ]; then
