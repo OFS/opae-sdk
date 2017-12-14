d402ddbf5af6a43016b982e51a495d38ae91f4bf
Rodrigo Rojo <rodrigo.rojo@intel.com>
2017-12-13 17:13:10 -0800

    Change mock test script to build in Debug mode

--- scripts/test-gtapi-mock-drv.sh
+++ scripts/test-gtapi-mock-drv.sh
@@ -5,8 +5,7 @@ pushd mybuild_gtapi_mock_drv
 
 trap "popd" EXIT
 
-cmake .. -DBUILD_TESTS=ON
+cmake .. -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
 make
-
-make test
+CTEST_OUTPUT_ON_FAILURE=1 make test
 echo "test-gtapi-mock-drv build PASSED"
