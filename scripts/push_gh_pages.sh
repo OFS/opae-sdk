e287a9fb99645bd9df734ae1c043221a412c81ad
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-19 13:48:33 -0800

    Place github.io pages push script in separate file

--- scripts/push_gh_pages.sh
+++ scripts/push_gh_pages.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Exit with nonzero exit code if anything fails
+set -e
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+SHA=`git rev-parse --verify HEAD`
+
 if [ -n "$GITHUB_API_KEY" ]; then
   cd mybuild_docs/sphinx/html
   git init
