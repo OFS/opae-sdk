2a6095ee14c4f892e3ee4089ee17826716a1b3a0
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-19 15:50:45 -0800

    use test repository

--- scripts/push_gh_pages.sh
+++ scripts/push_gh_pages.sh
@@ -9,7 +9,7 @@ TARGET_BRANCH="gh-pages"
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    exit 0
+    # exit 0
 fi
 
 SHA=`git rev-parse --verify HEAD`
@@ -22,5 +22,5 @@ if [ -n "$GITHUB_API_KEY" ]; then
   git -c user.name='Abelardo Jara-Berrocal' -c user.email='abelardo.jara-berrocal@intel.com' commit -m init
   # Make sure to make the output quiet, or else the API token will leak!
   # API key can replace the password.
-  git push -f -q https://abelardojarab:$GITHUB_API_KEY@github.com/OPAE/opae-sdk gh-pages &2>/dev/null
+  git push -f -q https://abelardojarab:$GITHUB_API_KEY@github.com/abelardojarab/opae-doc-test gh-pages &2>/dev/null
 fi
