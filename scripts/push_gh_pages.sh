d129f48b808bffbba62380e2fe95e6d864a6f75f
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2017-12-19 15:38:28 -0800

    correct repository to push to

--- scripts/push_gh_pages.sh
+++ scripts/push_gh_pages.sh
@@ -22,5 +22,5 @@ if [ -n "$GITHUB_API_KEY" ]; then
   git -c user.name='Abelardo Jara-Berrocal' -c user.email='abelardo.jara-berrocal@intel.com' commit -m init
   # Make sure to make the output quiet, or else the API token will leak!
   # API key can replace the password.
-  git push -f -q https://abelardojarab:$GITHUB_API_KEY@opae.github.io gh-pages &2>/dev/null
+  git push -f -q https://abelardojarab:$GITHUB_API_KEY@github.com/OPAE/opae-sdk gh-pages &2>/dev/null
 fi
