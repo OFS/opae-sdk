25cc325faa913fdaaf78e364887321a3ea181390
Ru Pan <ru.pan@intel.com>
2018-01-17 14:59:58 -0800

    Detect packager dependency packages information during build

---
+++ cmake/modules/Findjsonschema.cmake
@@ -0,0 +1,20 @@
+# - Try to find python jsonschema package
+# Once done, this will define
+#
+#  jsonschema_FOUND - system has python jsonschema & repoze packages
+#  jsonschema_INCLUDE_DIRS - the jsonschema include directories
+
+
+#find python site-package installation path
+execute_process ( COMMAND python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()" OUTPUT_VARIABLE PYTHON_SITE_PACKAGES OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+find_path(jsonschema_INCLUDE_DIRS
+  NAMES jsonschema 
+  PATHS ${PYTHON_SITE_PACKAGES}
+  DOC "packager runtime dependencies: jsonschema")
+
+if(jsonschema_INCLUDE_DIRS)
+  set(jsonschema_FOUND true)
+else(jsonschema_INCLUDE_DIRS)
+  message(WARNING "PACKAGER runtime depenency python jsonschema package is missing!!!")
+endif(jsonschema_INCLUDE_DIRS)
