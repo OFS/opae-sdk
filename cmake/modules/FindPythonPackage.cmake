e2ea7af6a27c8b9b691c29443163d70a3fa8b01d
Ru Pan <ru.pan@intel.com>
2018-01-18 22:51:48 -0800

    Fix variable name in command

--- cmake/modules/FindPythonPackage.cmake
+++ cmake/modules/FindPythonPackage.cmake
@@ -5,7 +5,7 @@
 macro(FIND_PYTHON_PKG PKG_NAME )
 
     set(STATUS_FLAG ${PKG_NAME}_NOT_FOUND)
-    set(EXECUTE_COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE      jsonschema_NOT_FOUND)
+    set(EXECUTE_COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE  ${STATUS_FLAG})
 
     execute_process( COMMAND ${EXECUTE_COMMAND})
 
