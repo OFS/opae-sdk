be5d410d6df78ec10fba0b70c22e6e9f773d3122
Ru Pan <ru.pan@intel.com>
2018-01-18 22:54:40 -0800

    Remove extra line in macro

--- cmake/modules/FindPythonPackage.cmake
+++ cmake/modules/FindPythonPackage.cmake
@@ -1,9 +1,6 @@
 # - Find python package installation status
-#
-#
 
 macro(FIND_PYTHON_PKG PKG_NAME )
-
     set(STATUS_FLAG ${PKG_NAME}_NOT_FOUND)
     set(EXECUTE_COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE  ${STATUS_FLAG})
 
