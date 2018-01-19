abb7c0f60867958c9d6534ec1e2a407b9b9e73e8
Ru Pan <ru.pan@intel.com>
2018-01-19 15:27:15 -0800

    Detect packager app run time dependency information during build (#166)
    * Detect packager dependency packages information during build

---
+++ cmake/modules/FindPythonPackage.cmake
@@ -0,0 +1,15 @@
+# - Find python package installation status
+#
+# ERR_LEVEL: 
+#     SEND_ERROR     = CMake Error, continue processing 
+#     WARNING        = CMake Warning, continue processing
+#     FATAL_ERROR    = CMake Error, stop processing and generation
+#
+
+macro(FIND_PYTHON_PKG PKG_NAME ERR_LEVEL )
+    execute_process( COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE  STATUS_FLAG)
+
+    if(STATUS_FLAG)
+        message(${ERR_LEVEL} "PACKAGER depenency python ${PKG_NAME} package is missing!!!")
+    endif(STATUS_FLAG)
+endmacro(FIND_PYTHON_PKG)
