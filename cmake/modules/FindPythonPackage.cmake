3ed518a6a1e87b960fa41aab1aa119660bdaaff5
Ru Pan <ru.pan@intel.com>
2018-01-18 22:45:45 -0800

    Use import python package to determine installation status

---
+++ cmake/modules/FindPythonPackage.cmake
@@ -0,0 +1,15 @@
+# - Find python package installation status
+#
+#
+
+macro(FIND_PYTHON_PKG PKG_NAME )
+
+    set(STATUS_FLAG ${PKG_NAME}_NOT_FOUND)
+    set(EXECUTE_COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE      jsonschema_NOT_FOUND)
+
+    execute_process( COMMAND ${EXECUTE_COMMAND})
+
+    if(STATUS_FLAG)
+        message(WARNING "PACKAGER runtime depenency python ${PKG_NAME} package is missing!!!")
+    endif(STATUS_FLAG)
+endmacro(FIND_PYTHON_PKG)
