144523d44ee641503f511a97aa127a8db0470ece
Ru Pan <ru.pan@intel.com>
2018-01-19 00:02:33 -0800

    Fix string issue in command

--- cmake/modules/FindPythonPackage.cmake
+++ cmake/modules/FindPythonPackage.cmake
@@ -1,10 +1,7 @@
 # - Find python package installation status
 
 macro(FIND_PYTHON_PKG PKG_NAME )
-    set(STATUS_FLAG ${PKG_NAME}_NOT_FOUND)
-    set(EXECUTE_COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE  ${STATUS_FLAG})
-
-    execute_process( COMMAND ${EXECUTE_COMMAND})
+    execute_process( COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE  STATUS_FLAG)
 
     if(STATUS_FLAG)
         message(WARNING "PACKAGER runtime depenency python ${PKG_NAME} package is missing!!!")
