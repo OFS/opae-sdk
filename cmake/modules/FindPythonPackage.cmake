846c58d8aee964bdb2e2f68d60f8cb068ce4aaf5
Ru Pan <ru.pan@intel.com>
2018-01-19 11:22:14 -0800

    Add error level option in macro

--- cmake/modules/FindPythonPackage.cmake
+++ cmake/modules/FindPythonPackage.cmake
@@ -1,9 +1,15 @@
 # - Find python package installation status
+#
+# ERR_LEVEL: 
+#     SEND_ERROR     = CMake Error, continue processing 
+#     WARNING        = CMake Warning, continue processing
+#     FATAL_ERROR    = CMake Error, stop processing and generation
+#
 
-macro(FIND_PYTHON_PKG PKG_NAME )
+macro(FIND_PYTHON_PKG PKG_NAME ERR_LEVEL )
     execute_process( COMMAND python -c "import ${PKG_NAME}" ERROR_QUIET RESULT_VARIABLE  STATUS_FLAG)
 
     if(STATUS_FLAG)
-        message(WARNING "PACKAGER runtime depenency python ${PKG_NAME} package is missing!!!")
+        message(${ERR_LEVEL} "PACKAGER depenency python ${PKG_NAME} package is missing!!!")
     endif(STATUS_FLAG)
 endmacro(FIND_PYTHON_PKG)
