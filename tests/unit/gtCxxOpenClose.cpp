185e0794baed510e5e66a3cf8aff47ec94ab89fa
Rodrigo Rojo <rodrigo.rojo@me.com>
2018-02-02 16:21:25 -0800

    opaec++: Add prototype of accelerator classs (#204)
    * opaec++: Add prototype of accelerator classs
    
    This adds/updates:
    * first version of accelerator with `enumerate` and `open` functions
    * hello_fpga-2.cpp to only use implemented functions in accelerator
    *  Unit test for accelerator::open
    * libopae-cxx.so library

--- tests/unit/gtCxxOpenClose.cpp
+++ tests/unit/gtCxxOpenClose.cpp
@@ -10,22 +10,39 @@ extern "C" {
 #include <opae/cxx/core/token.h>
 #include <opae/cxx/core/handle.h>
 #include <opae/cxx/core/properties.h>
+#include <opae/cxx/accelerator.h>
 
 using namespace opae::fpga::types;
+using namespace opae::fpga::resource;
 
 /**
- * @test cxx_enum_01
+ * @test handle_open_01
  * Given an environment with at least one accelerator<br>
- * When I call accelerator::enumerate with a filter of only FPGA_ACCELERATOR<br>
+ * When I call token::enumerate with a filter of only FPGA_ACCELERATOR<br>
  * And I call handle::open with a token<br>
  * Then I get a non-null handle<br>
- * And no exceptions are thrown
+ * And no exceptions are thrown when I release the handle and tokens<br>
  */
-TEST(CxxOpen, open_01) {
+TEST(CxxOpen, handle_open_01) {
   auto tokens = token::enumerate({FPGA_ACCELERATOR});
-  EXPECT_TRUE(tokens.size() > 0);
+  ASSERT_TRUE(tokens.size() > 0);
   handle::ptr_t h = handle::open(tokens[0], FPGA_OPEN_SHARED);
   ASSERT_NE(nullptr, h.get());
   h.reset();
   ASSERT_NO_THROW(tokens.clear());
+}
+
+/**
+ * @test accelerator_open_01
+ * Given an environment with at least one accelerator<br>
+ * When I call accelerator::enumerate with a filter of only FPGA_ACCELERATOR<br>
+ * And I call accelerator::open using an accelerator instance<br>
+ * And I release the accelerator objects<br>
+ * Then no exceptions are thrown<br>
+ */
+TEST(CxxOpen, accelerator_open_01) {
+  auto accelerators = accelerator::enumerate({});
+  ASSERT_TRUE(accelerators.size() > 0);
+  ASSERT_NO_THROW(accelerators[0]->open(FPGA_OPEN_SHARED));
+  ASSERT_NO_THROW(accelerators.clear());
 }
