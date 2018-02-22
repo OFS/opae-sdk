32eeb465a5fe8608cfedc9807d2fbb715a3649f8
scottgi <scott.gibbons@intel.com>
2018-02-22 14:43:41 -0800

    Trying pylint disabling

--- tools/packager/packager.py
+++ tools/packager/packager.py
@@ -75,8 +75,9 @@ def run_packager():
         print(USAGE)
 
     elif args.cmd == "version":
-        print("{0}: version {1}".format(DESCRIPTION,
-                                        version.get_full_version())) # pylint: disable=no-member
+        print("{0}: version {1}"
+	      .format(DESCRIPTION,
+	              version.get_full_version()))  # pylint: disable=no-member
     elif args.cmd == "create-gbs":
         subparser.usage = "\n" + cmd_description + \
             " --rbf=<RBF_PATH> --afu-json=<AFU_JSON_PATH>"\
