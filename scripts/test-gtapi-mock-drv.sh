99b3299cc4ef6d87a17864249af26518d035f0a0
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-03-07 16:46:02 -0800

    Add fpgad to be build and run during mock testing

--- scripts/test-gtapi-mock-drv.sh
+++ scripts/test-gtapi-mock-drv.sh
@@ -6,7 +6,8 @@ pushd mybuild_gtapi_mock_drv
 trap "popd" EXIT
 
 cmake .. -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
-make mock gtapi
-
+make mock gtapi fpgad
+./bin/fpgad -d
 make test
+kill $(cat /tmp/fpgad.pid)
 echo "test-gtapi-mock-drv build PASSED"
