0019213f158ee483d04c0af4fb0eb104badb1f6a
Enno Luebbers <enno.luebbers@intel.com>
2018-03-15 13:16:09 -0700

    Add first functional test

---
+++ tests/function/gtError.cpp
@@ -0,0 +1,67 @@
+// Copyright(c) 2018, Intel Corporation
+//
+// Redistribution  and  use  in source  and  binary  forms,  with  or  without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of  source code  must retain the  above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// * Neither the name  of Intel Corporation  nor the names of its contributors
+//   may be used to  endorse or promote  products derived  from this  software
+//   without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+// IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
+// LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
+// CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
+// SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
+// INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
+// CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include <opae/error.h>
+
+#include "common_test.h"
+#include "gtest/gtest.h"
+#include "types_int.h"
+
+using namespace common_test;
+
+/**
+ * @test       error_01
+ *
+ * @brief      When passed a valid token, the combination of fpgaGetProperties()
+ *             fpgaPropertiesGetNumErrors(), fpgaPropertiesGetErrorInfo() and
+ *             fpgaReadError() is able to print the status of all error registers.
+ *
+ */
+TEST(LibopaecErrorCommonALL, error_01) {
+  struct _fpga_token _t;
+  fpga_token t = &_t;
+  fpga_properties p;
+  fpga_error_info info;
+  unsigned int n = 0;
+  unsigned int i = 0;
+  uint64_t val = 0;
+
+  // generate token
+  token_for_afu0(&_t);
+
+  // get number of error registers
+  ASSERT_EQ(FPGA_OK, fpgaGetProperties(t, &p));
+  ASSERT_EQ(FPGA_OK, fpgaPropertiesGetNumErrors(p, &n));
+
+  // for each error register, get info and read the current value  
+  for (i = 0; i < n; i++) {
+    // get info struct for error register
+    ASSERT_EQ(FPGA_OK, fpgaGetErrorInfo(t, i, &info));
+    EXPECT_EQ(FPGA_OK, fpgaReadError(t, i, &val));
+    printf("[%d] %s: 0x%16lX\n", i, info.name, val);
+  }
+
+}
