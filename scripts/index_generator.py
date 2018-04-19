0bfb9a6aba085c7ce87619fe3de425c71033294a
Nakul Korde <nakul.korde@intel.com>
2018-04-19 10:13:19 -0700

    Modify documentation landing page
    * Beautify the landing page
    * Sort the version number links

--- scripts/index_generator.py
+++ scripts/index_generator.py
@@ -11,15 +11,28 @@ from jinja2 import Environment
 
 index_tpl = """
 <html>
-<title>{% block title %}Open Programmable Acceleration Engine{% endblock %}
+<head>
+<link rel="stylesheet" href="https://opae.github.io/latest/_static/css/theme.css" type="text/css" />
+<title>Open Programmable Acceleration Engine
 </title>
-<body bgcolor='#85C1E9'>
-<h1>Open Programmable Acceleration Engine</h1>
-<ul>
-{% for ver in versions %}
+</head>
+<body style="background-color: white">
+<div class="section" style="background-color: white; padding: 50px; position: absolute; left: 50%; margin-right: -50%; transform: translate(-50%, 0%) ">
+
+<h1>Open Programmable Acceleration Engine - Documentation</h1>
+
+<hr/>
+
+<p>Please select one of the links below to browse the online OPAE documentation:</p>
+
+<ul style="font-size: large; font-weight: bold">
+
+  {% for ver in versions %}
   <li><a href="{{ ver }}/index.html">{{ ver }}</a></li>
 {% endfor %}
+
 </ul>
+</div>
 </body>
 </html>"""
 
@@ -30,7 +43,12 @@ def main():
         if os.path.isdir(x):
             if x != ".git":
                 versions.append(x)
-    versions.sort()
+    def splitit(s):
+        try:
+           return map(int, s.split('.'))
+        except:
+            return s
+    versions.sort(key=splitit)
     t = Environment().from_string(index_tpl)
     f = open('index.html', 'w')
     f.write(t.render(versions=versions))
