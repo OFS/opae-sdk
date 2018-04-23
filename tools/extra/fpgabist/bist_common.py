19286c92866585bf0224ee24d8244fc6140719b8
jabuie <jon.buie@intel.com>
2018-04-23 10:59:30 -0700

    remove distutils

--- tools/extra/fpgabist/bist_common.py
+++ tools/extra/fpgabist/bist_common.py
@@ -24,7 +24,6 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from distutils.spawn import find_executable
 import glob
 import os
 import re
