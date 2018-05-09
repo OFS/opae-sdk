3820e483a522c858418a5d1d4e742bbe4027bef4
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-05-09 16:49:45 +0000

    Use pybind11 Python module for building pyopae
    * Remove use of External_Project in pyopae/CMakeLists.txt
    * Replace with pybind11 Python module
      * Call pybind11.get_include()
    * Change setup.py to not be a little smarter about include paths
      * Try to use pybind11 module for finding inlude path for pybind11
      * Warn if opae not included in include paths
      * TODO: Try to find OPAE include paths if not already in paths

--- pyopae/setup.py
+++ pyopae/setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 from setuptools.command.build_ext import build_ext
 from distutils.extension import Extension
@@ -8,13 +9,31 @@ def override_build_extensions(self):
     original_build_extensions(self)
 build_ext.build_extensions = override_build_extensions
 
+def get_include_dirs(*args):
+    include_dirs = list(set(args))
+    try:
+        import pybind11
+    except ImportError:
+        print "Couldn't import pybind11"
+        return include_dirs
+    else:
+        pybind_include = pybind11.get_include()
+        if not os.path.exists(os.path.join(pybind_include, 'pybind11')):
+            pybind_include = pybind11.get_include(True)
+        include_dirs.append(pybind_include)
+    if not any([os.path.exists(os.path.join(inc, 'opae'))
+                for inc in include_dirs]):
+        print "Could not find OPAE in any include paths"
+    return include_dirs
+
+
 extensions = [
     Extension("opae._opae",
               sources=["opae.cpp"],
               language="c++",
               extra_compile_args=["-std=c++11"],
               extra_link_args=["-std=c++11"],
-              include_dirs=["@CMAKE_INSTALL_PREFIX@/include"],
+              include_dirs=get_include_dirs("@CMAKE_INSTALL_PREFIX@/include"),
               libraries=["opae-c", "opae-cxx-core"],
               library_dirs=["@CMAKE_INSTALL_PREFIX@/lib"])
 ]
