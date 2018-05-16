2a5abd4278dcc98021b5e342fd030b8bc12004c3
Rodrigo Rojo <rodrigo.rojo@me.com>
2018-05-15 17:49:58 -0700

    opaecxx: Add os_object API to event class (#403)
    * opaecxx: Add os_object API to event class
    
    * Add a call to `fpgaGetOSObjectFromEventHandle` in event factory fn
    * Store that os object in the event object
    * Return that os object when calling `os_object` fn from an event object
    
    * Update events c'tor
    
    Initialize os_object_ to -1

--- tests/unit/gtCxxEvents.cpp
+++ tests/unit/gtCxxEvents.cpp
@@ -1,8 +1,10 @@
 #include "gtest/gtest.h"
 
-#include "opae/cxx/core/token.h"
-#include "opae/cxx/core/handle.h"
 #include "opae/cxx/core/events.h"
+#include "opae/cxx/core/handle.h"
+#include "opae/cxx/core/token.h"
+
+#include <fcntl.h>
 
 using namespace opae::fpga::types;
 
@@ -26,7 +28,6 @@ class CxxEvent_f1 : public ::testing::Test {
   handle::ptr_t accel_;
 };
 
-
 /**
  * @test register_event_01
  * Given an open accelerator handle object<br>
@@ -35,11 +36,9 @@ class CxxEvent_f1 : public ::testing::Test {
  * Then no exception is thrown<br>
  * And I get a non-null event shared pointer<br>
  */
-TEST_F(CxxEvent_f1, register_event_01){
+TEST_F(CxxEvent_f1, register_event_01) {
   event::ptr_t ev;
-  ASSERT_NO_THROW(
-      ev = event::register_event(accel_, event::type_t::error)
-      );
+  ASSERT_NO_THROW(ev = event::register_event(accel_, event::type_t::error));
   ASSERT_NE(nullptr, ev.get());
 }
 
@@ -51,10 +50,24 @@ TEST_F(CxxEvent_f1, register_event_01){
  * Then no exception is thrown<br>
  * And I get a non-null event shared pointer<br>
  */
-TEST_F(CxxEvent_f1, register_event_02){
+TEST_F(CxxEvent_f1, register_event_02) {
   event::ptr_t ev;
-  ASSERT_NO_THROW(
-      ev = event::register_event(accel_, FPGA_EVENT_ERROR);
-      );
+  ASSERT_NO_THROW(ev = event::register_event(accel_, FPGA_EVENT_ERROR););
   ASSERT_NE(nullptr, ev.get());
+}
+
+/**
+ * @test get_os_object
+ * Given an open accelerator handle object<br>
+ * And an event object created with event::register_event()<br>
+ * When I call event::os_object using the event object<br>
+ * Then I get a valid file descriptor for polling on the event<br>
+ */
+TEST_F(CxxEvent_f1, get_os_object) {
+  event::ptr_t ev;
+  ASSERT_NO_THROW(ev = event::register_event(accel_, FPGA_EVENT_ERROR););
+  ASSERT_NE(nullptr, ev.get());
+  auto fd = ev->os_object();
+  auto res = fcntl(fd, F_GETFL);
+  ASSERT_NE(res, -1);
 }
