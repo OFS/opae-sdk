d2b40b262435d32add76c87ba0ec1f136c71e07e
Michael Adler <Michael.Adler@intel.com>
2018-05-27 12:54:11 -0400

    Move pipeline registers to the AFU side of CCI-P clock crossing.

--- platforms/platform_if/rtl/platform_shims/platform_shim_ccip.sv
+++ platforms/platform_if/rtl/platform_shims/platform_shim_ccip.sv
@@ -61,9 +61,7 @@ module platform_shim_ccip
 
     //
     // Has the AFU JSON requested a clock crossing for the CCI-P signals?
-    // We compute this here because it affects register stage insertion.
     //
-
 `ifdef PLATFORM_PARAM_CCI_P_CLOCK_IS_DEFAULT
     // AFU asked for default clock.
     localparam CCI_P_CHANGE_CLOCK = 0;
@@ -75,74 +73,14 @@ module platform_shim_ccip
     localparam CCI_P_CHANGE_CLOCK = 1;
 `endif
 
-
-    // ====================================================================
-    //  CCI-P auto-register for timing
-    // ====================================================================
-
-    //
-    // Register CCI-P signals according to the platform's rules.  This is
-    // typically done to relieve timing pressure.  Note that adding latency
-    // to the almost full and transmit paths counts against the sending
-    // limit to the FIU.
-    //
-    logic reg_cp2af_softReset;
-    t_if_ccip_Tx reg_af2cp_sTx;
-    t_if_ccip_Rx reg_cp2af_sRx;
-    logic [1:0] reg_cp2af_pwrState;
-    logic reg_cp2af_error;
-
     //
     // How many register stages should be inserted for timing?
     //
-    function automatic int numTimingRegStages();
-        int n_stages = 0;
-
-        // Were timing registers requested in the AFU JSON?
 `ifdef PLATFORM_PARAM_CCI_P_ADD_TIMING_REG_STAGES
-        n_stages = `PLATFORM_PARAM_CCI_P_ADD_TIMING_REG_STAGES;
+    localparam NUM_TIMING_REG_STAGES = `PLATFORM_PARAM_CCI_P_ADD_TIMING_REG_STAGES;
+`else
+    localparam NUM_TIMING_REG_STAGES = 0;
 `endif
-
-        // Override the register request if a clock crossing is being
-        // inserted here.
-        if (CCI_P_CHANGE_CLOCK)
-        begin
-            // At least one stage is required
-            if (n_stages == 0) n_stages = 1;
-            // Use at least the recommended number of stages
-`ifdef PLATFORM_PARAM_CCI_P_SUGGESTED_TIMING_REG_STAGES
-            if (`PLATFORM_PARAM_CCI_P_SUGGESTED_TIMING_REG_STAGES > n_stages)
-            begin
-                n_stages = `PLATFORM_PARAM_CCI_P_SUGGESTED_TIMING_REG_STAGES;
-            end
-`endif
-        end
-
-        return n_stages;
-    endfunction
-
-    localparam NUM_TIMING_REG_STAGES = numTimingRegStages();
-
-    platform_utils_ccip_reg
-      #(
-        .N_REG_STAGES(NUM_TIMING_REG_STAGES)
-        )
-      ccip_reg
-       (
-        .clk(pClk),
-
-        .fiu_reset(pck_cp2af_softReset),
-        .fiu_cp2af_sRx(pck_cp2af_sRx),
-        .fiu_af2cp_sTx(pck_af2cp_sTx),
-        .fiu_cp2af_pwrState(pck_cp2af_pwrState),
-        .fiu_cp2af_error(pck_cp2af_error),
-
-        .afu_reset(reg_cp2af_softReset),
-        .afu_cp2af_sRx(reg_cp2af_sRx),
-        .afu_af2cp_sTx(reg_af2cp_sTx),
-        .afu_cp2af_pwrState(reg_cp2af_pwrState),
-        .afu_cp2af_error(reg_cp2af_error)
-        );
 
 
     // ====================================================================
@@ -150,24 +88,73 @@ module platform_shim_ccip
     //  AFU's JSON file.
     // ====================================================================
 
+    // CCI-P signals in the AFU's requested clock domain
+    logic cross_cp2af_softReset;
+    t_if_ccip_Tx cross_af2cp_sTx;
+    t_if_ccip_Rx cross_cp2af_sRx;
+    logic [1:0] cross_cp2af_pwrState;
+    logic cross_cp2af_error;
+
     generate
         if (CCI_P_CHANGE_CLOCK == 0)
         begin : nc
             // No clock crossing
             always_comb
             begin
-                afu_cp2af_softReset = reg_cp2af_softReset;
-                reg_af2cp_sTx = afu_af2cp_sTx;
-                afu_cp2af_sRx = reg_cp2af_sRx;
+                cross_cp2af_softReset = pck_cp2af_softReset;
+                pck_af2cp_sTx = cross_af2cp_sTx;
+                cross_cp2af_sRx = pck_cp2af_sRx;
 
-                afu_cp2af_pwrState = reg_cp2af_pwrState;
-                afu_cp2af_error = reg_cp2af_error;
+                cross_cp2af_pwrState = pck_cp2af_pwrState;
+                cross_cp2af_error = pck_cp2af_error;
             end
         end
         else
         begin : c
+            // Before crossing add some FIU-side register stages for timing.
+            logic reg_cp2af_softReset;
+            t_if_ccip_Tx reg_af2cp_sTx;
+            t_if_ccip_Rx reg_cp2af_sRx;
+            logic [1:0] reg_cp2af_pwrState;
+            logic reg_cp2af_error;
+
+            // How many register stages should be inserted for timing?
+`ifdef PLATFORM_PARAM_CCI_P_SUGGESTED_TIMING_REG_STAGES
+            // At least one stage, perhaps more.
+            localparam NUM_PRE_CROSS_REG_STAGES =
+                (`PLATFORM_PARAM_CCI_P_SUGGESTED_TIMING_REG_STAGES != 0) ?
+                    `PLATFORM_PARAM_CCI_P_SUGGESTED_TIMING_REG_STAGES : 1;
+`else
+            localparam NUM_PRE_CROSS_REG_STAGES = 1;
+`endif
+
+            platform_utils_ccip_reg
+              #(
+                .N_REG_STAGES(NUM_PRE_CROSS_REG_STAGES)
+                )
+            ccip_pre_cross_reg
+               (
+                .clk(pClk),
+
+                .fiu_reset(pck_cp2af_softReset),
+                .fiu_cp2af_sRx(pck_cp2af_sRx),
+                .fiu_af2cp_sTx(pck_af2cp_sTx),
+                .fiu_cp2af_pwrState(pck_cp2af_pwrState),
+                .fiu_cp2af_error(pck_cp2af_error),
+
+                .afu_reset(reg_cp2af_softReset),
+                .afu_cp2af_sRx(reg_cp2af_sRx),
+                .afu_af2cp_sTx(reg_af2cp_sTx),
+                .afu_cp2af_pwrState(reg_cp2af_pwrState),
+                .afu_cp2af_error(reg_cp2af_error)
+                );
+
             // Cross to the target clock
-            platform_utils_ccip_async_shim ccip_async_shim
+            platform_utils_ccip_async_shim
+              #(
+                .EXTRA_ALMOST_FULL_STAGES(2 * NUM_TIMING_REG_STAGES)
+                )
+              ccip_async_shim
                (
                 .bb_softreset(reg_cp2af_softReset),
                 .bb_clk(pClk),
@@ -176,16 +163,52 @@ module platform_shim_ccip
                 .bb_pwrState(reg_cp2af_pwrState),
                 .bb_error(reg_cp2af_error),
 
-                .afu_softreset(afu_cp2af_softReset),
+                .afu_softreset(cross_cp2af_softReset),
                 .afu_clk(afu_clk),
-                .afu_tx(afu_af2cp_sTx),
-                .afu_rx(afu_cp2af_sRx),
-                .afu_pwrState(afu_cp2af_pwrState),
-                .afu_error(afu_cp2af_error),
+                .afu_tx(cross_af2cp_sTx),
+                .afu_rx(cross_cp2af_sRx),
+                .afu_pwrState(cross_cp2af_pwrState),
+                .afu_error(cross_cp2af_error),
 
                 .async_shim_error()
                 );
         end
     endgenerate
+
+
+    // ====================================================================
+    //  Add CCI-P register stages for timing, as requested by AFU JSON.
+    //
+    //  For AFUs with both register stages and a clock crossing, we
+    //  add register stages on the AFU side. Extra space is left in the
+    //  clock crossing FIFO so that the almost full contract with the AFU
+    //  remains unchanged, despite the added latency of almost full and
+    //  the extra requests in flight.
+    //
+    //  NOTE: When no clock crossing is instantiated, register stages
+    //  added here count against the almost full sending limits!
+    //
+    // ====================================================================
+
+    platform_utils_ccip_reg
+      #(
+        .N_REG_STAGES(NUM_TIMING_REG_STAGES)
+        )
+      ccip_reg
+       (
+        .clk(afu_clk),
+
+        .fiu_reset(cross_cp2af_softReset),
+        .fiu_cp2af_sRx(cross_cp2af_sRx),
+        .fiu_af2cp_sTx(cross_af2cp_sTx),
+        .fiu_cp2af_pwrState(cross_cp2af_pwrState),
+        .fiu_cp2af_error(cross_cp2af_error),
+
+        .afu_reset(afu_cp2af_softReset),
+        .afu_cp2af_sRx(afu_cp2af_sRx),
+        .afu_af2cp_sTx(afu_af2cp_sTx),
+        .afu_cp2af_pwrState(afu_cp2af_pwrState),
+        .afu_cp2af_error(afu_cp2af_error)
+        );
 
 endmodule // platform_shim_std_ccip
