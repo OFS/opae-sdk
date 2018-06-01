89bbd56e1cae197c961f966d2010bf60986af4f3
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-06-01 10:12:18 -0700

    pyopae: update unit tests for properties.get
    Update the unit tests to use the properties.get factory method when creating
    properties.

--- pyopae/test_pyopae.py
+++ pyopae/test_pyopae.py
@@ -33,25 +33,47 @@ NLB0 = "d8424dc4-a4a3-c413-f89e-433683f9040b"
 
 def test_guid():
     """test_guid test guid property"""
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     guid_str = props.guid
     guid = uuid.UUID(guid_str)
     assert str(guid).lower() == NLB0
 
 
+def test_set_objtype_accelerator():
+    """test_set_objtype test setting object type to FPGA_ACCELERATOR"""
+    props = opae.api.properties.get(type=opae.api.FPGA_ACCELERATOR)
+    assert props.type == opae.api.FPGA_ACCELERATOR, "object type is not FPGA_ACCELERATOR"
+
+def test_set_objtype_device():
+    """test_set_objtype test setting object type to FPGA_DEVICE"""
+    props = opae.api.properties.get(type=opae.api.FPGA_DEVICE)
+    assert props.type == opae.api.FPGA_DEVICE, "object type is not FPGA_DEVICE"
+
+def test_set_bus():
+    """test_set_bus"""
+    props = opae.api.properties.get(bus=0x5e)
+    assert props.bus == 0x5e, "bus is not 0x5e"
+
+def test_set_device():
+    """test_set_device"""
+    props = opae.api.properties.get(device=0xe)
+    assert props.device == 0xe, "device is not 0xe"
+
+def test_set_function():
+    """test_set_function"""
+    props = opae.api.properties.get(function=0x7)
+    assert props.function == 0x7, "function is not 0x7"
+
 def test_enumerate():
     """test_enumerate test if we can enumerate for NLB0 using GUID"""
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     toks = opae.api.token.enumerate([props])
     assert toks
 
 
 def test_open():
     """test_open test if we can enumerate/open NLB0 using GUID"""
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     toks = opae.api.token.enumerate([props])
     assert toks
     resource = opae.api.handle.open(toks[0], opae.api.FPGA_OPEN_SHARED)
@@ -62,8 +84,7 @@ def test_mmio():
     """test_mmio sweep through CSRs writing random values and reading from
     them. NOTE: This should NOT be run on real hardware.
     """
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     toks = opae.api.token.enumerate([props])
     assert toks
     resource = opae.api.handle.open(toks[0], opae.api.FPGA_OPEN_SHARED)
@@ -80,8 +101,7 @@ def test_mmio():
 
 def test_buffer():
     """test_buffer test shared buffer allocation"""
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     toks = opae.api.token.enumerate([props])
     assert toks
     resource = opae.api.handle.open(toks[0], opae.api.FPGA_OPEN_SHARED)
@@ -93,8 +113,7 @@ def test_buffer():
 def test_close():
     """test_close test if we can enumerate/open and then close an accelerator
     handle"""
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     toks = opae.api.token.enumerate([props])
     assert toks
     resource = opae.api.handle.open(toks[0], opae.api.FPGA_OPEN_SHARED)
@@ -106,13 +125,12 @@ def test_close():
 def test_version():
     """test_version test the version API"""
     ver = opae.api.version()
-    assert ver == (0, 13, 1)
+    assert ver == (1, 0, 0)
 
 
 def test_register_event():
     """test_event test that we can register for an event"""
-    props = opae.api.properties()
-    props.guid = NLB0
+    props = opae.api.properties.get(guid=NLB0)
     toks = opae.api.token.enumerate([props])
     assert toks
     resource = opae.api.handle.open(toks[0], opae.api.FPGA_OPEN_SHARED)
