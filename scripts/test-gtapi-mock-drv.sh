69a485931694bea618d0921f96ea75d58b031b6b
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-06-19 18:16:32 -0700

    pyopae: Fix OPAE Python API testing
    * Add nose2 to pip install in travis
    * Change test-gtapi-mock-drv to activate virtual env.

--- scripts/test-gtapi-mock-drv.sh
+++ scripts/test-gtapi-mock-drv.sh
@@ -6,9 +6,11 @@ pushd mybuild_gtapi_mock_drv
 trap "popd" EXIT
 
 cmake .. -DBUILD_TESTS=ON -DCMAKE_BUILD_TYPE=Debug
-make mock gtapi fpgad
+. bin/activate
+make mock gtapi fpgad pyopae
 LD_PRELOAD="$PWD/lib/libmock.so" ./bin/fpgad -d
 CTEST_OUTPUT_ON_FAILURE=1 make test
 kill $(cat /tmp/fpgad.pid)
+deactivate
 
 echo "test-gtapi-mock-drv build PASSED"
