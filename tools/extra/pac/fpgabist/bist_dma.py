fd9c01732f7304e3a2fa5acc0534b9d8b078cfe6
Tara Renduchintala <trenduch@sj-nx35.altera.com>
2018-06-27 11:08:51 -0700

    Add a bus number to bist_dma to implement change from fpga_dma_test

--- tools/extra/pac/fpgabist/bist_dma.py
+++ tools/extra/pac/fpgabist/bist_dma.py
@@ -34,8 +34,8 @@ import bist_common as bc
 class DmaMode(bc.BistMode):
     name = "dma_afu"
 
-    def __init__(self):
-        self.executables = {'fpga_dma_test': '0'}
+    def __init__(self, bus_num):
+        self.executables = {'fpga_dma_test': '-B '+ str(bus_num) + ' 0'}
 
     def run(self, gbs_path, bus_num):
         bc.load_gbs(gbs_path, bus_num)
