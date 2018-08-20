56e9f8e3a99d63841ea35a11208cbdc1035b901c
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-08-20 10:43:11 -0700

    plugin-api: restructured directories

---
+++ libopae/pluginmgr.c
@@ -0,0 +1,11 @@
+/*
+ * pluginmgr.c
+ * Copyright (C) 2018 rrojo <rrojo@ub-rojo-vm-04.amr.corp.intel.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "pluginmgr.h"
+
+
+
---
+++ libopae/pluginmgr.h
@@ -0,0 +1,13 @@
+/*
+ * pluginmgr.h
+ * Copyright (C) 2018 rrojo <rrojo@ub-rojo-vm-04.amr.corp.intel.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef PLUGINMGR_H
+#define PLUGINMGR_H
+
+
+
+#endif /* !PLUGINMGR_H */
