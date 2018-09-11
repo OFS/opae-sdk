41bf6908a0c809e808cb9e0c0eaa783f1519ecaa
Nakul Korde <nakul.korde@intel.com>
2018-09-11 23:38:59 +0000

    CI: Fix output checking condition

--- scripts/test-codingstyle-cpp.sh
+++ scripts/test-codingstyle-cpp.sh
@@ -5,7 +5,7 @@ pushd $(dirname $0)
 FILES=$(find ../libopaecxx/src ../libopaecxx/samples ../common/include/opae/cxx/core \( -iname "*.cpp" -or -iname "*.h" \))
 clang-format-3.9 -i -style=Google $FILES
 
-if [ "git diff" != NULL ]; then
+if [ x$(git diff) != x ]; then
    echo "Coding style issues occured. Please fix them based on the following suggestions. You can run clang-format on these files in order to automatically format your code." 
    git --no-pager diff
    echo "The files that need to be fixed are:"
