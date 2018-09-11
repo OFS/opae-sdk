d0b72777015a8d5104008d9014c1f5a637de5328
Nakul Korde <nakul.korde@intel.com>
2018-09-11 16:59:41 -0700

    CI: Update wording

--- scripts/test-codingstyle-cpp.sh
+++ scripts/test-codingstyle-cpp.sh
@@ -5,8 +5,8 @@ pushd $(dirname $0)
 FILES=$(find ../libopaecxx/src ../libopaecxx/samples ../common/include/opae/cxx/core \( -iname "*.cpp" -or -iname "*.h" \))
 clang-format-3.9 -i -style=Google $FILES
 
-if [ x$(git diff) != x ]; then
-   echo "Coding style issues occured. Please fix them based on the following suggestions. You can run clang-format on these files in order to automatically format your code." 
+if [ x"$(git diff)" != x ]; then
+   echo "Coding style check failed. Please fix them based on the following suggestions. You can run clang-format on these files in order to automatically format your code." 
    git --no-pager diff
    echo "The files that need to be fixed are:"
    git diff --name-only
