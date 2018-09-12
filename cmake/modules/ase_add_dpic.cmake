2e6c6c48c4adc4615981e4ad33837f9bfce9488b
Tracy Zhang <tracy.l.zhang@intel.com>
2018-09-12 18:27:15 +0000

    ASE: remove the extra ASE_LL_VIEW flag

--- cmake/modules/ase_add_dpic.cmake
+++ cmake/modules/ase_add_dpic.cmake
@@ -87,9 +87,8 @@ if(BUILD_ASE_TESTS)
     SIMULATOR=${ASE_SIMULATOR}
     ${used_platform}
 	ASE_LL_VIEW=1
-	ASE_LL_VIEW=1
 	ASE_MSG_VIEW=1
-	ASE_UNIT=1)
+        ASE_UNIT=1)
 else()
   # Add required compilation flags
   target_compile_definitions(opae-c-ase-server-${name}
