c1b3c6e54afd4f1adad5fc80537bb905c75f4699
Barraza, IsaacX <isaacx.barraza@intel.com>
2018-09-18 12:38:00 -0700

    Added bitstream test cases.

--- testing/xfpga/test_metadata_c.cpp
+++ testing/xfpga/test_metadata_c.cpp
@@ -196,6 +196,12 @@ TEST_P(metadata_c, read_gbs_metadata) {
   // Valid metadata
   result = read_gbs_metadata(bitstream_valid, &gbs_metadata);
   EXPECT_EQ(result, FPGA_OK);
+
+  test_system::instance()->invalidate_malloc();
+
+  // Valid metadata - malloc fail
+  result = read_gbs_metadata(bitstream_valid, &gbs_metadata);
+  EXPECT_EQ(result, FPGA_NO_MEMORY);
   
   // Invalid metadata afu-image node
   result = read_gbs_metadata(bitstream_no_afu_image, &gbs_metadata);
@@ -236,6 +242,12 @@ TEST_P(metadata_c, validate_bitstream_metadata) {
   // Valid metadata
   result = validate_bitstream_metadata(handle_, bitstream_valid);
   EXPECT_EQ(result, FPGA_OK);
+
+  test_system::instance()->invalidate_malloc();
+
+  // Valid metadata - malloc fail
+  result = validate_bitstream_metadata(handle_, bitstream_valid);
+  EXPECT_EQ(result, FPGA_NO_MEMORY);
 
   // Invalid input bitstream
   result = validate_bitstream_metadata(handle_, bitstream_invalid_guid);
