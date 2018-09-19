c54c52400070b2d44b4018fe0ad6366157c6117a
Barraza, IsaacX <isaacx.barraza@intel.com>
2018-09-19 12:55:10 -0700

    Added userclk unit tests.

--- testing/xfpga/test_usrclk_c.cpp
+++ testing/xfpga/test_usrclk_c.cpp
@@ -41,9 +41,56 @@ extern "C" {
 #include "gtest/gtest.h"
 #include "types_int.h"
 #include "test_system.h"
-
+#include "xfpga.h"
 
 using namespace opae::testing;
+
+class usrclk_c
+    : public ::testing::TestWithParam<std::string> {
+ protected:
+  usrclk_c() : tmpsysfs_("mocksys-XXXXXX"), handle_dev_(nullptr), handle_accel_(nullptr) {}
+
+  virtual void SetUp() override {
+    ASSERT_TRUE(test_platform::exists(GetParam()));
+    platform_ = test_platform::get(GetParam());
+    system_ = test_system::instance();
+    system_->initialize();
+    tmpsysfs_ = system_->prepare_syfs(platform_);
+
+    ASSERT_EQ(xfpga_fpgaGetProperties(nullptr, &filter_dev_), FPGA_OK);
+    ASSERT_EQ(fpgaPropertiesSetObjectType(filter_dev_, FPGA_DEVICE), FPGA_OK);
+    ASSERT_EQ(xfpga_fpgaEnumerate(&filter_dev_, 1, tokens_dev_.data(), 
+              tokens_dev_.size(), &num_matches_), FPGA_OK);
+
+    ASSERT_EQ(xfpga_fpgaGetProperties(nullptr, &filter_accel_), FPGA_OK);
+    ASSERT_EQ(fpgaPropertiesSetObjectType(filter_accel_, FPGA_ACCELERATOR), FPGA_OK);
+    ASSERT_EQ(xfpga_fpgaEnumerate(&filter_accel_, 1, tokens_accel_.data(),
+              tokens_accel_.size(), &num_matches_), FPGA_OK);
+  }
+
+  virtual void TearDown() override {
+    EXPECT_EQ(fpgaDestroyProperties(&filter_dev_), FPGA_OK);
+    EXPECT_EQ(fpgaDestroyProperties(&filter_accel_), FPGA_OK);
+    if (handle_dev_ != nullptr) EXPECT_EQ(xfpga_fpgaClose(handle_dev_), FPGA_OK);
+    if (handle_accel_ != nullptr) EXPECT_EQ(xfpga_fpgaClose(handle_accel_), FPGA_OK);
+    if (!tmpsysfs_.empty() && tmpsysfs_.size() > 1) {
+      std::string cmd = "rm -rf " + tmpsysfs_;
+      std::system(cmd.c_str());
+    }
+    system_->finalize();
+  }
+
+  std::string tmpsysfs_;
+  fpga_properties filter_dev_;
+  fpga_properties filter_accel_;
+  std::array<fpga_token, 2> tokens_dev_;
+  std::array<fpga_token, 2> tokens_accel_;
+  fpga_handle handle_dev_;
+  fpga_handle handle_accel_;
+  uint32_t num_matches_;
+  test_platform platform_;
+  test_system *system_;
+};
 
 /**
 * @test    afu_usrclk_01
@@ -79,3 +126,139 @@ TEST(usrclk_c, afu_usrclk_01) {
   fv_BugLog(2);
 
 }
+
+/**
+* @test    set_user_clock
+* @brief   Tests: set_userclock
+* @details When the sysfs path is NULL, set_userclock
+*          returns FPGA_INVALID_PARAM.
+*/
+TEST(usrclk_c, set_userclock) {
+  fpga_result result;
+
+  // Null handle
+  result = set_userclock(NULL, 0, 0);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+}
+
+/**
+* @test    get_user_clock
+* @brief   Tests: get_userclock
+* @details When the sysfs path is NULL, get_userclock
+*          returns FPGA_INVALID_PARAM.
+*/
+TEST(usrclk_c, get_userclock) {
+  fpga_result result;
+
+  // Null handle
+  result = get_userclock(NULL, 0, 0);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+}
+
+/**
+* @test    fi_run_initz
+* @brief   Tests: fi_RunInitz
+* @details When the sysfs path is NULL, fi_RunInitz
+*          returns -1.
+*/
+TEST(usrclk_c, fi_run_initz) {
+  EXPECT_EQ(-1, fi_RunInitz(NULL));
+}
+
+/**
+* @test    fpga_set_user_clock
+* @brief   Tests: fpgaSetUserClock
+* @details fpgaSetUserClock
+*/
+TEST_P(usrclk_c, fpga_set_user_clock) {
+  fpga_result result;
+  int flags = 0;
+
+  // Null handle
+  result = xfpga_fpgaSetUserClock(NULL, 0, 0, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Invalid object type
+  ASSERT_EQ(FPGA_OK, xfpga_fpgaOpen(tokens_dev_[0], &handle_dev_, 0));
+
+  // Invalid clk
+  result = xfpga_fpgaSetUserClock(handle_dev_, 0, 0, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);  
+
+  // Valid clk
+  result = xfpga_fpgaSetUserClock(handle_dev_, 312, 156, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Valid object type
+  ASSERT_EQ(FPGA_OK, xfpga_fpgaOpen(tokens_accel_[0], &handle_accel_, 0));
+
+  // Invalid clk
+  result = xfpga_fpgaSetUserClock(handle_accel_, 0, 0, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Valid clk
+  result = xfpga_fpgaSetUserClock(handle_accel_, 312, 156, flags);
+  EXPECT_EQ(result, FPGA_NOT_SUPPORTED);
+
+  struct _fpga_handle  *_handle = (struct _fpga_handle *)handle_accel_;
+  int fddev = _handle->fddev;
+
+  // Token not found
+  _handle->token = NULL;
+  result = xfpga_fpgaSetUserClock(handle_accel_, 312, 156, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Invalid file handle descriptor
+  _handle->fddev = -1;
+  result = xfpga_fpgaSetUserClock(handle_accel_, 312, 156, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  _handle->fddev = fddev;
+}
+
+/**
+* @test    fpga_get_user_clock
+* @brief   Tests: fpgaGetUserClock
+* @details fpgaGetUserClock
+*/
+TEST_P(usrclk_c, fpga_get_user_clock) {
+  fpga_result result;
+  uint64_t high;
+  uint64_t low;
+  int flags = 0;
+
+  // Null handle
+  result = xfpga_fpgaGetUserClock(NULL, &high, &low, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Invalid object type
+  ASSERT_EQ(FPGA_OK, xfpga_fpgaOpen(tokens_dev_[0], &handle_dev_, 0));
+
+  // Valid params, invalid object type
+  result = xfpga_fpgaGetUserClock(handle_dev_, &high, &low, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Valid object type
+  ASSERT_EQ(FPGA_OK, xfpga_fpgaOpen(tokens_accel_[0], &handle_accel_, 0));
+
+  // Valid params, valid object type
+  result = xfpga_fpgaGetUserClock(handle_accel_, &high, &low, flags);
+  EXPECT_EQ(result, FPGA_OK);
+
+  struct _fpga_handle  *_handle = (struct _fpga_handle *)handle_accel_;
+  int fddev = _handle->fddev;
+
+  // Token not found
+  _handle->token = NULL;
+  result = xfpga_fpgaGetUserClock(handle_accel_, &high, &low, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  // Invalid file handle descriptor
+  _handle->fddev = -1;
+  result = xfpga_fpgaGetUserClock(handle_accel_, &high, &low, flags);
+  EXPECT_EQ(result, FPGA_INVALID_PARAM);
+
+  _handle->fddev = fddev;
+}
+
+INSTANTIATE_TEST_CASE_P(usrclk, usrclk_c, ::testing::ValuesIn(test_platform::keys(true)));
