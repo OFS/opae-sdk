8646d7752d7de69d2bf7f33fff87706bce060a8e
scottgi <scott.gibbons@intel.com>
2018-09-21 11:33:04 -0700

    Fix ACCEL with no DEVICE

--- tools/base/fpgainfo/fpgainfo.c
+++ tools/base/fpgainfo/fpgainfo.c
@@ -49,23 +49,26 @@ void fpgainfo_print_common(const char *hdr, fpga_properties props)
 {
 	fpga_result res = FPGA_OK;
 	char guid_str[38];
-	uint64_t object_id;
-	uint8_t bus;
-	uint16_t segment;
-	uint8_t device;
-	uint8_t function;
-	uint16_t device_id;
-	uint8_t socket_id;
-	fpga_guid guid;
-	fpga_guid port_guid;
-	uint32_t num_slots;
-	uint64_t bbs_id;
-	fpga_version bbs_version;
-	fpga_objtype objtype;
+	uint64_t object_id = 0;
+	uint8_t bus = 0;
+	uint16_t segment = 0;
+	uint8_t device = 0;
+	uint8_t function = 0;
+	uint16_t device_id = 0;
+	uint8_t socket_id = 0;
+	fpga_guid guid = {0};
+	fpga_guid port_guid = {0};
+	uint32_t num_slots = 0;
+	uint64_t bbs_id = 0;
+	fpga_version bbs_version = {0};
+	fpga_objtype objtype = -1;
 	fpga_properties pprops = props;
 	fpga_token par = NULL;
 	int is_accelerator = 0;
 	bool has_parent = true;
+
+	res = fpgaPropertiesGetObjectType(props, &objtype);
+	fpgainfo_print_err("reading objtype from properties", res);
 
 	res = fpgaPropertiesGetObjectID(props, &object_id);
 	fpgainfo_print_err("reading object_id from properties", res);
@@ -82,14 +85,8 @@ void fpgainfo_print_common(const char *hdr, fpga_properties props)
 	res = fpgaPropertiesGetFunction(props, &function);
 	fpgainfo_print_err("reading function from properties", res);
 
-	res = fpgaPropertiesGetSocketID(props, &socket_id);
-	fpgainfo_print_err("reading socket_id from properties", res);
-
 	res = fpgaPropertiesGetDeviceID(props, &device_id);
 	fpgainfo_print_err("reading device_id from properties", res);
-
-	res = fpgaPropertiesGetObjectType(props, &objtype);
-	fpgainfo_print_err("reading objtype from properties", res);
 
 	if (objtype != FPGA_DEVICE) {
 		res = fpgaPropertiesGetGUID(props, &port_guid);
@@ -128,14 +125,19 @@ void fpgainfo_print_common(const char *hdr, fpga_properties props)
 	res = fpgaPropertiesGetGUID(pprops, &guid);
 	fpgainfo_print_err("reading guid from properties", res);
 
-	res = fpgaPropertiesGetNumSlots(pprops, &num_slots);
-	fpgainfo_print_err("reading num_slots from properties", res);
+	if (objtype == FPGA_DEVICE) {
+		res = fpgaPropertiesGetNumSlots(pprops, &num_slots);
+		fpgainfo_print_err("reading num_slots from properties", res);
 
-	res = fpgaPropertiesGetBBSID(pprops, &bbs_id);
-	fpgainfo_print_err("reading bbs_id from properties", res);
+		res = fpgaPropertiesGetBBSID(pprops, &bbs_id);
+		fpgainfo_print_err("reading bbs_id from properties", res);
 
-	res = fpgaPropertiesGetBBSVersion(pprops, &bbs_version);
-	fpgainfo_print_err("reading bbs_version from properties", res);
+		res = fpgaPropertiesGetSocketID(props, &socket_id);
+		fpgainfo_print_err("reading socket_id from properties", res);
+
+		res = fpgaPropertiesGetBBSVersion(pprops, &bbs_version);
+		fpgainfo_print_err("reading bbs_version from properties", res);
+	}
 
 	// TODO: Implement once model and capabilities accessors are
 	// implemented
@@ -162,14 +164,18 @@ void fpgainfo_print_common(const char *hdr, fpga_properties props)
 	printf("%-29s : %04X:%02X:%02X:%01X\n", "PCIe s:b:d:f", segment, bus,
 	       device, function);
 	printf("%-29s : 0x%04X\n", "Device Id", device_id);
-	printf("%-29s : 0x%02X\n", "Socket Id", socket_id);
-	printf("%-29s : %02d\n", "Ports Num", num_slots);
-	printf("%-29s : 0x%" PRIX64 "\n", "Bitstream Id", bbs_id);
-	printf("%-29s : 0x%lX\n", "Bitstream Version",
-	       *(uint64_t *)&bbs_version);
-	uuid_unparse(guid, guid_str);
-	printf("%-29s : %s\n", "Pr Interface Id", guid_str);
+	if (objtype == FPGA_DEVICE) {
+		printf("%-29s : 0x%02X\n", "Socket Id", socket_id);
+		printf("%-29s : %02d\n", "Ports Num", num_slots);
+		printf("%-29s : 0x%" PRIX64 "\n", "Bitstream Id", bbs_id);
+		printf("%-29s : 0x%lX\n", "Bitstream Version",
+		       *(uint64_t *)&bbs_version);
+	}
 
+	if (objtype == FPGA_DEVICE) {
+		uuid_unparse(guid, guid_str);
+		printf("%-29s : %s\n", "Pr Interface Id", guid_str);
+	}
 	if (is_accelerator) {
 		uuid_unparse(port_guid, guid_str);
 		printf("%-29s : %s\n", "Accelerator Id", guid_str);
