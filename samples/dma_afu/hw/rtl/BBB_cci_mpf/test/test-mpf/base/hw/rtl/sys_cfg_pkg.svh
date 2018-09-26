c6ac7aabf0b3704ee1ec7880904078ccd9e2c2c5
Abelardo Jara-Berrocal <abelardo.jara-berrocal@intel.com>
2018-09-26 15:24:03 -0700

    Bug: remove MPF from sample code

--- samples/dma_afu/hw/rtl/BBB_cci_mpf/test/test-mpf/base/hw/rtl/sys_cfg_pkg.svh
+++
@@ -1,8 +0,0 @@
-`ifndef SYS_CFG_PKG_SV                                          
-    `define SYS_CFG_PKG_SV      
-//  `define CCIP_DEBUG              // Add ccip_debug_module
-    `define VENDOR_ALTERA           // Use Altera FPGA
-    `define TOOL_QUARTUS            // Use Altera Quartus Tools     
-`endif
- 
-
