89761bcbe7a7248f82a7be92061bae2fee8c57cb
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-10-12 14:41:37 -0700

    Update testing infrastructure
    * Add testing/platform directory with fpga_hw class
      * This is a singleton class that discovers fpga hw when it is first
      created
      * This gets built into a library called libfpga_db.so
    * Move test_platform/test_device structs to fpga_hw.h|cpp files
    * Rename friendly name skx-p-1s to just skx-p
      * Updated test fixtures that referenced it
    * Update mock sysfs archive for skx to use 640 bbs_id and bbs_metadata

--- testing/xfpga/test_usrclk_c.cpp
+++ testing/xfpga/test_usrclk_c.cpp
@@ -277,4 +277,4 @@ TEST_P(usrclk_c, fpga_get_user_clock) {
 
 // TODO: Fix user clock test for DCP
 //INSTANTIATE_TEST_CASE_P(usrclk, usrclk_c, ::testing::ValuesIn(test_platform::keys(true)));
-INSTANTIATE_TEST_CASE_P(usrclk, usrclk_c, ::testing::Values("skx-p-1s"));
+INSTANTIATE_TEST_CASE_P(usrclk, usrclk_c, ::testing::Values("skx-p"));
