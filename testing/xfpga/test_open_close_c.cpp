6dd6fddb71f895f3e776ffa5b65245cc526a3277
jelonanx <jelonx.anderson@intel.com>
2018-10-22 10:00:31 -0700

    Added review suggestions

--- testing/xfpga/test_open_close_c.cpp
+++ testing/xfpga/test_open_close_c.cpp
@@ -316,8 +316,8 @@ TEST_P(openclose_c_p, close_03) {
  *             already opened token returns FPGA_OK.
  */
 TEST_P(openclose_c_p, open_share) {
-  fpga_handle h1 = NULL;
-  fpga_handle h2 = NULL;
+  fpga_handle h1 = nullptr;
+  fpga_handle h2 = nullptr;
 
   EXPECT_EQ(FPGA_OK, xfpga_fpgaOpen(tokens_[0], &h1, FPGA_OPEN_SHARED));
   EXPECT_EQ(FPGA_OK, xfpga_fpgaOpen(tokens_[0], &h2, FPGA_OPEN_SHARED));
@@ -340,20 +340,6 @@ class openclose_c_mock_p
  protected:
   openclose_c_mock_p() {}
 
-//  virtual void SetUp() override {
-//    ASSERT_TRUE(test_platform::exists(GetParam()));
-//    platform_ = test_platform::get(GetParam());
-//    system_ = test_system::instance();
-//    system_->initialize();
-//    system_->prepare_syfs(platform_);
-//  }
-//
-//  virtual void TearDown() override {
-//    system_->finalize();
-//  }
-//
-//  test_platform platform_;
-//  test_system *system_;
 };
 
 TEST_P(openclose_c_mock_p, invalid_open_close) {
