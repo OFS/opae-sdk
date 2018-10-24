f7495e74926be4f64261fa319e0bb34ac4bf726b
Tim Whisonant <tim.whisonant@intel.com>
2018-10-24 16:16:46 -0700

    cmake: remove NEW_UNITS, rely on BUILD_TESTS

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -6,7 +6,7 @@ mkdir -p unittests
 cd unittests
 
 if [ ! -f CMakeCache.txt ]; then
-	cmake .. -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON -DNEW_UNITS=ON -DBUILD_LIBOPAE_PY=ON
+	cmake .. -DCMAKE_BUILD_TYPE=Coverage -DBUILD_TESTS=ON -DBUILD_LIBOPAE_PY=ON
 fi
 
 mkdir -p coverage_files
