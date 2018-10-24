97b7228ef9c7f1ed412ca69c776d49a15a883d45
jelonanx <jelonx.anderson@intel.com>
2018-10-24 15:58:18 -0700

    Modify tests to work on hw

--- testing/opae-c/test_buffer_c.cpp
+++ testing/opae-c/test_buffer_c.cpp
@@ -62,10 +62,11 @@ class buffer_c_p : public ::testing::TestWithParam<std::string> {
     ASSERT_EQ(fpgaInitialize(NULL), FPGA_OK);
     ASSERT_EQ(fpgaGetProperties(nullptr, &filter_), FPGA_OK);
     ASSERT_EQ(fpgaPropertiesSetObjectType(filter_, FPGA_ACCELERATOR), FPGA_OK);
+    ASSERT_EQ(fpgaPropertiesSetDeviceID(filter_, 
+              platform_.devices[0].device_id), FPGA_OK);
     num_matches_ = 0;
     ASSERT_EQ(fpgaEnumerate(&filter_, 1, tokens_.data(), tokens_.size(),
-                            &num_matches_),
-              FPGA_OK);
+                            &num_matches_), FPGA_OK);
     EXPECT_EQ(num_matches_, platform_.devices.size());
     accel_ = nullptr;
     ASSERT_EQ(fpgaOpen(tokens_[0], &accel_, 0), FPGA_OK);
@@ -132,4 +133,4 @@ TEST_P(buffer_c_p, ioaddr) {
   EXPECT_EQ(fpgaReleaseBuffer(accel_, wsid), FPGA_OK);
 }
 
-INSTANTIATE_TEST_CASE_P(buffer_c, buffer_c_p, ::testing::ValuesIn(test_platform::keys(true)));
+INSTANTIATE_TEST_CASE_P(buffer_c, buffer_c_p, ::testing::ValuesIn(test_platform::platforms({})));
