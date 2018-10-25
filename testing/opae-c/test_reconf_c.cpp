7b01d1d2ced78e79d07d622c8659385629d4369a
Jelonanx <jelonx.anderson@intel.com>
2018-10-25 15:31:57 -0700

    Remove num_matches assertion

--- testing/opae-c/test_reconf_c.cpp
+++ testing/opae-c/test_reconf_c.cpp
@@ -60,14 +60,13 @@ class reconf_c_p : public ::testing::TestWithParam<std::string> {
     system_->prepare_syfs(platform_);
     invalid_device_ = test_device::unknown();
 
+    filter_ = nullptr;
     ASSERT_EQ(fpgaInitialize(NULL), FPGA_OK);
     ASSERT_EQ(fpgaGetProperties(nullptr, &filter_), FPGA_OK);
     ASSERT_EQ(fpgaPropertiesSetObjectType(filter_, FPGA_ACCELERATOR), FPGA_OK);
     num_matches_ = 0;
     ASSERT_EQ(fpgaEnumerate(&filter_, 1, tokens_.data(), tokens_.size(),
-                            &num_matches_),
-              FPGA_OK);
-    EXPECT_EQ(num_matches_, platform_.devices.size());
+                            &num_matches_), FPGA_OK);
     accel_ = nullptr;
     ASSERT_EQ(fpgaOpen(tokens_[0], &accel_, 0), FPGA_OK);
   }
@@ -108,4 +107,5 @@ TEST_P(reconf_c_p, pr) {
 		  bitstream, 5, 0), FPGA_INVALID_PARAM);
 }
 
-INSTANTIATE_TEST_CASE_P(reconf_c, reconf_c_p, ::testing::ValuesIn(test_platform::keys(true)));
+INSTANTIATE_TEST_CASE_P(reconf_c, reconf_c_p,
+                        ::testing::ValuesIn(test_platform::platforms({})));
