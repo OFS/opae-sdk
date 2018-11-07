ae7c0fb67362ba5849472ac404f9fa361e20bbcf
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-11-06 21:51:15 -0800

    pydiag: fix codingstyle filter, python tests env

--- scripts/cover-py.sh
+++ scripts/cover-py.sh
@@ -13,5 +13,5 @@ echo "Making pyopae"
 make -j4 opae-c opae-cxx-core pyopae-dist
 
 python -m pip install pyopae/stage/dist/opae.fpga*.whl --user
-python -m nose2 -F -C --coverage-report term-missing test_fpgadiag -s tools/extra/pyfpgadiag/stage --coverage tools/extra/pyfpgadiag/stage
+LD_LIBRARY_PATH=$PWD/lib python -m nose2 -F -C --coverage-report term-missing test_fpgadiag -s tools/extra/pyfpgadiag/stage --coverage tools/extra/pyfpgadiag/stage
 pip uninstall opae.fpga
