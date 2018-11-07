86cfc0c4ace420124cb2d2c0ffdbd67dc99e0e78
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-11-06 18:02:07 -0800

    pydiag: Add tests to travis as a job
    Add cover-py.sh script which builds opae.fpga and tests opae.fpgadiag
    Add travis job to call script

---
+++ scripts/cover-py.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+set -o xtrace
+
+mkdir -p pytests
+cd pytests
+
+if [ ! -f CMakeCache.txt ]; then
+	cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_LIBOPAE_PY=ON -DBUILD_PYTHON_DIST=ON
+fi
+
+echo "Making pyopae"
+make -j4 pyopae-dist
+
+python -m pip install pybind11 nose2 mock numpy
+pip install pyopae/stage/dist/opae.fpga*.whl --user
+python -m nose2 -F -C --coverage-report term-missing test_fpgadiag -s tools/extra/pyfpgadiag/stage --coverage tools/extra/pyfpgadiag/stage
+pip uninstall opae.fpga
