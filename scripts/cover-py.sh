8df11bc50b6dcbf30cbd3862e57d904e128ce6af
Rodrigo Rojo <rodrigo.rojo@intel.com>
2018-11-06 21:35:27 -0800

    pydiag: fix codingstyle/coverage script

--- scripts/cover-py.sh
+++ scripts/cover-py.sh
@@ -10,9 +10,8 @@ if [ ! -f CMakeCache.txt ]; then
 fi
 
 echo "Making pyopae"
-make -j4 pyopae-dist
+make -j4 opae-c opae-cxx-core pyopae-dist
 
-python -m pip install pybind11 nose2 mock numpy
-pip install pyopae/stage/dist/opae.fpga*.whl --user
+python -m pip install pyopae/stage/dist/opae.fpga*.whl --user
 python -m nose2 -F -C --coverage-report term-missing test_fpgadiag -s tools/extra/pyfpgadiag/stage --coverage tools/extra/pyfpgadiag/stage
 pip uninstall opae.fpga
