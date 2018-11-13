0a6e70c9fbdd1fc79a4b0054e150c2d536ee11a1
Tracy Zhang <tracy.l.zhang@intel.com>
2018-11-13 12:01:07 -0800

    ASE: Fixed the ASE build issue after OPAE test changes (#935)
    * ASE: Fixed the ASE build issue after OPAE test changes
    
    * ASE: make ASE coverage run on external github

--- ase/api/src/close.c
+++ ase/api/src/close.c
@@ -23,10 +23,9 @@
 // CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
-#endif				// HAVE_CONFIG_H
+#endif
 
 #include <opae/access.h>
 #include "common_int.h"
