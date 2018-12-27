bd3f4974a3fbfc4c5f5d44503c2af9f9ea264cf5
Huang, Wei <wei.huang@intel.com>
2018-12-27 14:28:38 +0800

    Vista creek tools alpha (#556)
    * Enable DMA unaligned pointer and size test on all DDRs.

--- tools/extra/pac/fpgabist/dma/fpga_dma_test.c
+++ tools/extra/pac/fpgabist/dma/fpga_dma_test.c
@@ -777,15 +777,15 @@ int main(int argc, char *argv[])
 	if (!use_ase) {
 		printf("Running DDR sweep test\n");
 		res = ddr_sweep(dma_h, config.target.size, 0, 0);
-	#if 0	// skip unaligned DDR sweep test temporarily for Vista Creek board
-		printf("DDR sweep with unaligned pointer and size\n");
-		res |= ddr_sweep(dma_h, config.target.size, 61, 5);
-		res |= ddr_sweep(dma_h, config.target.size, 3, 0);
-		res |= ddr_sweep(dma_h, config.target.size, 7, 3);
-		res |= ddr_sweep(dma_h, config.target.size, 0, 3);
-		res |= ddr_sweep(dma_h, config.target.size, 0, 61);
-		res |= ddr_sweep(dma_h, config.target.size, 0, 7);
-	#endif
+		if (config.target.dma >= 0 && config.target.dma < 3) {
+			printf("DDR sweep with unaligned pointer and size\n");
+			res |= ddr_sweep(dma_h, config.target.size, 61, 5);
+			res |= ddr_sweep(dma_h, config.target.size, 3, 0);
+			res |= ddr_sweep(dma_h, config.target.size, 7, 3);
+			res |= ddr_sweep(dma_h, config.target.size, 0, 3);
+			res |= ddr_sweep(dma_h, config.target.size, 0, 61);
+			res |= ddr_sweep(dma_h, config.target.size, 0, 7);
+		}
 		ON_ERR_GOTO(res, out_dma_close, "ddr_sweep");
 	}
 
