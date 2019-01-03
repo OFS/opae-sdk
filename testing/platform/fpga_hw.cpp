ad0ac7894080e1d8824251c060c17bac0484b6c4
Barraza, IsaacX <isaacx.barraza@intel.com>
2019-01-03 15:02:55 -0800

    Fixed test platform not removing invalid platforms when running on hw.

--- testing/platform/fpga_hw.cpp
+++ testing/platform/fpga_hw.cpp
@@ -174,10 +174,11 @@ std::vector<std::string> test_platform::platforms(
   }
   // from the list of platform names requested, remove the ones not found in
   // the platform db
-  std::remove_if(keys.begin(), keys.end(), [](const std::string &n) {
+  keys.erase(
+    std::remove_if(keys.begin(), keys.end(), [](const std::string &n) {
       auto db = fpga_db::instance();
       return !db->exists(n);
-  });
+  }), keys.end());
   return keys;
 }
 
