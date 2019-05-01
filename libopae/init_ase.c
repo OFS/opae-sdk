22d6e51fa51ba5afa36027e95888898da62a542c
Tracy Zhang <tracy.l.zhang@intel.com>
2019-05-01 15:48:38 -0700

    Adding search path for opae_ase.cfg

--- libopae/init_ase.c
+++ libopae/init_ase.c
@@ -27,44 +27,87 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif // HAVE_CONFIG_H
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdarg.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <linux/limits.h>
+#include <pthread.h>
+#include <pwd.h>
+#include <unistd.h>
+
+#include "safe_string/safe_string.h"
 
 #include <opae/init.h>
 #include <opae/utils.h>
 #include "pluginmgr.h"
 #include "opae_int.h"
-#undef __USE_GNU
 
-#include "safe_string/safe_string.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#ifndef __USE_GNU
-#define __USE_GNU
-#endif // __USE_GNU
-#include <pthread.h>
+#define HOME_CFG_PATHS 3
+STATIC const char *_opae_home_cfg_files[HOME_CFG_PATHS] = {
+	"/.local/opae_ase.cfg",
+	"/.local/opae/opae_ase.cfg",
+	"/.config/opae/opae_ase.cfg",
+};
+#define SYS_CFG_PATHS 2
+STATIC const char *_opae_sys_cfg_files[SYS_CFG_PATHS] = {
+	"/usr/local/etc/opae/opae_ase.cfg",
+	"/etc/opae/opae_ase.cfg",
+};
 
-#define PATH_MAX 1024
+// Find the canonicalized configuration file. If null, the file was not found.
+// Otherwise, it's the first configuration file found from a list of possible
+// paths. Note: The char * returned is allocated here, caller must free.
+STATIC char *find_ase_cfg()
+{
+	int i = 0;
+	char *file_name = NULL;
+	char home_cfg[PATH_MAX] = {0};
+	char *home_cfg_ptr = &home_cfg[0];
+	// get the user's home directory
+	struct passwd *user_passwd = getpwuid(getuid());
+
+	// first look in possible paths in the users home directory
+	for (i = 0; i < HOME_CFG_PATHS; ++i) {
+		if (strcpy_s(home_cfg, PATH_MAX, user_passwd->pw_dir)) {
+			OPAE_ERR("error copying pw_dir string");
+			return NULL;
+		}
+		home_cfg_ptr = home_cfg + strlen(home_cfg);
+		if (strcpy_s(home_cfg_ptr, PATH_MAX, _opae_home_cfg_files[i])) {
+			OPAE_ERR("error copying opae cfg dir string: %s",
+				 _opae_home_cfg_files[i]);
+			return NULL;
+		}
+		file_name = canonicalize_file_name(home_cfg);
+		if (file_name) {
+			return file_name;
+		} else {
+			home_cfg[0] = '\0';
+		}
+	}
+	// now look in possible system paths
+	for (i = 0; i < SYS_CFG_PATHS; ++i) {
+		strcpy_s(home_cfg, PATH_MAX, _opae_sys_cfg_files[i]);
+		file_name = canonicalize_file_name(home_cfg);
+		if (file_name) {
+			return file_name;
+		}
+	}
+	return NULL;
+}
 
 __attribute__((constructor)) STATIC void opae_ase_init(void)
 {
-	char cfg_path[PATH_MAX];
-	char *opae_path = getenv("OPAE_PLATFORM_ROOT");
-	if (strcpy_s(cfg_path, PATH_MAX, opae_path) != EOK) {
-		fprintf(stderr,
-				"Could not copy the opae_path successfully.\n");
-		return;
-	}
+	char *cfg_path = find_ase_cfg();
 
-	if (strcat_s(cfg_path, PATH_MAX, "/include/opae_ase.cfg") != EOK) {
-		fprintf(stderr,
-				"Could not copy the cfg_path successfully.\n");
-		return;
-	}
 	setenv("OPAE_EXPLICIT_INITIALIZE", "yes", 0);
 
 	fpgaInitialize(cfg_path);
+	free(cfg_path);
 }
 
 __attribute__((destructor)) STATIC void opae_ase_release(void)
