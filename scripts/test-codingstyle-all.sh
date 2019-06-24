f5187e5f30fd7c798eef09595b4d6f3e059f300f
Michael Adler <Michael.Adler@intel.com>
2019-06-24 17:43:50 -0400

    Update tests: (#1317)
    - ASE sources moved under libopae
    - Add scripts/.gitignore to ignore files downloaded by test scripts

--- scripts/test-codingstyle-all.sh
+++ scripts/test-codingstyle-all.sh
@@ -5,7 +5,7 @@ check_c () {
     pushd $(dirname $0)
 
     CHECKPATCH=checkpatch.pl
-    FILES=$(find ../libopae ../common/include/opae ../ase/api/src ../ase/sw ../tools/base/fpgaconf ../tools/base/fpgad \( -iname "*.c" -or -iname "*.h" \) -and \( ! -name "srv.c" \) -and \( ! -path "../libopae/plugins/xfpga/usrclk/*" \) -and \( ! -path "../common/include/opae/cxx/*" \) )
+    FILES=$(find ../libopae ../common/include/opae ../tools/base/fpgaconf ../tools/base/fpgad \( -iname "*.c" -or -iname "*.h" \) -and \( ! -name "srv.c" \) -and \( ! -path "../libopae/plugins/xfpga/usrclk/*" \) -and \( ! -path "../common/include/opae/cxx/*" \) )
 
     if [ ! -f $CHECKPATCH ]; then
         wget --no-check-certificate https://raw.githubusercontent.com/torvalds/linux/master/scripts/checkpatch.pl
