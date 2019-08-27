e17fa4d880a139c2de8ffe8153d528541e630501
Whisonant, Tim <tim.whisonant@intel.com>
2019-08-27 11:01:34 -0700

    opae.admin: import version classes from super-rsu

--- python/opae.admin/opae/admin/utils/utils.py
+++ python/opae.admin/opae/admin/utils/utils.py
@@ -25,6 +25,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 import os
 import sys
+from ctypes import c_uint64, LittleEndianStructure, Union
+from opae.admin.utils.log import LOG
 
 if sys.version_info[0] == 3:
     str_type = str
@@ -85,3 +87,156 @@ def dry_run(obj, enabled=True, **kwargs):
         if callable(obj):
             return dry_run_func(obj, **kwargs)
     return obj
+
+
+class max10_or_nios_version_bits(LittleEndianStructure):
+    _fields_ = [
+                   ("patch", c_uint64, 8),
+                   ("minor", c_uint64, 8),
+                   ("major", c_uint64, 8),
+                   ("revision", c_uint64, 8),
+                   ("reserved", c_uint64, 32)
+    ]
+
+class max10_or_nios_version(Union):
+    _fields_ = [("bits", max10_or_nios_version_bits),
+                ("value", c_uint64)]
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        val = '{}.{}.{}'.format(self.bits.major,
+                                self.bits.minor,
+                                self.bits.patch)
+        return val
+
+    def __repr__(self):
+        rev = chr(self.bits.revision)
+        val = str(self)
+        if rev.isalpha():
+            val += ' Revision {} '.format(rev)
+        return val
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __ne__(self, other):
+        return str(self) != str(other)
+
+    def __ge__(self, other):
+        self_tup = (self.major, self.minor, self.patch)
+        other_tup = (other.major, other.minor, other.patch)
+        return self_tup >= other_tup
+
+    def __le__(self, other):
+        self_tup = (self.major, self.minor, self.patch)
+        other_tup = (other.major, other.minor, other.patch)
+        return self_tup <= other_tup
+
+    @property
+    def major(self):
+        return self.bits.major
+
+    @property
+    def minor(self):
+        return self.bits.minor
+
+    @property
+    def patch(self):
+        return self.bits.patch
+
+    @property
+    def revision(self):
+        rev = chr(self.bits.revision)
+        if not rev.isalpha():
+            return '?'
+        return rev
+
+
+class d5005_bitstream_id_bits(LittleEndianStructure):
+    _fields_ = [
+                    ("githash", c_uint64, 32),
+                    ("hssi", c_uint64, 4),
+                    ("reserved36", c_uint64, 12),
+                    ("debug", c_uint64, 4),
+                    ("patch", c_uint64, 4),
+                    ("minor", c_uint64, 4),
+                    ("major", c_uint64, 4),
+    ]
+
+class d5005_fme_version(Union):
+    _fields_ = [("bits", d5005_bitstream_id_bits),
+                ("value", c_uint64)]
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        val = '{}.{}.{}'.format(self.bits.major,
+                                self.bits.minor,
+                                self.bits.patch)
+        return val
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __ne__(self, other):
+        return str(self) != str(other)
+
+    def __ge__(self, other):
+        self_tup = (self.major, self.minor, self.patch)
+        other_tup = (other.major, other.minor, other.patch)
+        return self_tup >= other_tup
+
+    def __le__(self, other):
+        self_tup = (self.major, self.minor, self.patch)
+        other_tup = (other.major, other.minor, other.patch)
+        return self_tup <= other_tup
+
+    @property
+    def major(self):
+        return self.bits.major
+
+    @property
+    def minor(self):
+        return self.bits.minor
+
+    @property
+    def patch(self):
+        return self.bits.patch
+
+
+class n3000_bitstream_id_bits(LittleEndianStructure):
+    _fields_ = [
+                    ("githash", c_uint64, 32),
+                    ("interface", c_uint64, 4),
+                    ("reserved36", c_uint64, 12),
+                    ("debug", c_uint64, 4),
+                    ("patch", c_uint64, 4),
+                    ("minor", c_uint64, 4),
+                    ("major", c_uint64, 4),
+    ]
+
+class n3000_fme_version(d5005_fme_version):
+    _fields_ = [("bits", n3000_bitstream_id_bits),
+                ("value", c_uint64)]
+
+
+def get_fme_version(vid_did, value):
+    """get_fme_version returns a version object suitable for the given device
+
+        Args:
+          vid_did: The PCIe vendor ID / device ID of the device in question.
+          value: The value of the version sysfs node to decode.
+    """
+    vid_did_to_cls = {(0x8086, 0x0b2b): d5005_fme_version,
+                      (0x8086, 0x0b30): n3000_fme_version}
+    try:
+        return vid_did_to_cls[vid_did](value)
+    except KeyError as exc:
+        LOG().exception('Unsupported platform: %s', vid_did)
+        raise
