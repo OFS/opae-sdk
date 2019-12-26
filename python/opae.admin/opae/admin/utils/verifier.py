ce98dec0ecfc69a63e61ce553084b1bc592d4503
Gibbons, Scott <scott.gibbons@intel.com>
2019-12-26 14:30:09 -0800

    Fix json.loads for Python 2.7.

--- python/opae.admin/opae/admin/utils/verifier.py
+++ python/opae.admin/opae/admin/utils/verifier.py
@@ -150,7 +150,7 @@ class print_bitstream(_VERIFIER_BASE):
         if not json_str:
             return
 
-        jstr = json.loads(json_str)
+        jstr = json.loads(str(json_str))
         print("JSON:\n")
         print(json.dumps(jstr, sort_keys=True, indent=4))
         print("")
