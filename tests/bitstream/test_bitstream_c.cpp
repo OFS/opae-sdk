ffcc61c120a55254d22160406c93b9ee694d0c9f
Tim Whisonant <tim.whisonant@intel.com>
2020-02-07 09:15:56 -0800

    Squashed 'opae-libs/' changes from 1aa584a..c6c0cdd
    c6c0cdd tests: use the full libbitstream/bitstream.h.
    
    git-subtree-dir: opae-libs
    git-subtree-split: c6c0cddd50bff0a37ffdc27b160784723eefd994

--- tests/bitstream/test_bitstream_c.cpp
+++ tests/bitstream/test_bitstream_c.cpp
@@ -24,7 +24,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include "bitstream.h"
+#include "libbitstream/bitstream.h"
 
 extern "C" {
 
--- tests/bitstream/test_metadatav1_c.cpp
+++ tests/bitstream/test_metadatav1_c.cpp
@@ -24,8 +24,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include "bitstream.h"
-#include "metadatav1.h"
+#include "libbitstream/bitstream.h"
+#include "libbitstream/metadatav1.h"
 
 extern "C" {
 
