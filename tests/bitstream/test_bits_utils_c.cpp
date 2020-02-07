c6c0cddd50bff0a37ffdc27b160784723eefd994
Tim Whisonant <tim.whisonant@intel.com>
2020-02-07 09:14:07 -0800

    tests: use the full libbitstream/bitstream.h.

--- tests/bitstream/test_bits_utils_c.cpp
+++ tests/bitstream/test_bits_utils_c.cpp
@@ -24,7 +24,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include "bits_utils.h"
+#include "libbitstream/bits_utils.h"
 
 extern "C" {
 
