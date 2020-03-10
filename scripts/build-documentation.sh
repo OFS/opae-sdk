dba8800f9626bdac469600ce01b2eba88ee27653
Nakul Korde <nakul.korde@intel.com>
2020-03-10 16:08:10 -0700

    Update documentation build

--- scripts/build-documentation.sh
+++ scripts/build-documentation.sh
@@ -9,7 +9,16 @@ if [[ $TEMP == *"fatal"* ]]; then
     git reset --hard origin/master
 fi
 
-pip install --user -r doc/sphinx/requirements.txt
+#generate virtual environment
+python -m virtualenv docs-build
+. docs-build/bin/activate
+
+#install requirements
+pip install -r doc/sphinx/requirements.txt
+if [ -d mybuild_docs ];
+then
+	rm -rf mybuild_docs
+fi
 
 mkdir mybuild_docs
 pushd mybuild_docs
@@ -20,7 +29,14 @@ cmake .. -DOPAE_BUILD_SPHINX_DOC=ON
 make docs
 
 
-echo "build Sphinx documentation FINISHED"
+if test $? -eq 0
+then
+    echo "Sphinx documentation build FINISHED"
+else
+    echo "Spinx documentation build FAILED"
+    exit 1
+fi
+
 
 
 if [[ $TEMP != *"fatal"* ]]; then
@@ -29,4 +45,6 @@ if [[ $TEMP != *"fatal"* ]]; then
 else
     echo "No new tag detected; Latest documentation will be uploaded to opae.github.io"
     /bin/bash ../scripts/push-documentation.sh latest
-fi    
+fi
+
+deactivate
