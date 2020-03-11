f64c04ac437712bdc022586fb87de0d6c1d610c4
Nakul Korde <nakul.korde@intel.com>
2020-03-11 11:54:32 -0700

    Resolve issues with documentation build (#1489)
    * Update documentation build
    
    * Move docs target after including opae-libs to preserve version number

--- scripts/build-documentation.sh
+++ scripts/build-documentation.sh
@@ -9,7 +9,12 @@ if [[ $TEMP == *"fatal"* ]]; then
     git reset --hard origin/master
 fi
 
-pip install --user -r doc/sphinx/requirements.txt
+#install requirements
+pip install -r doc/sphinx/requirements.txt
+if [ -d mybuild_docs ];
+then
+	rm -rf mybuild_docs
+fi
 
 mkdir mybuild_docs
 pushd mybuild_docs
@@ -20,7 +25,14 @@ cmake .. -DOPAE_BUILD_SPHINX_DOC=ON
 make docs
 
 
-echo "build Sphinx documentation FINISHED"
+if test $? -eq 0
+then
+    echo "Sphinx documentation build FINISHED"
+else
+    echo "Spinx documentation build FAILED"
+    exit 1
+fi
+
 
 
 if [[ $TEMP != *"fatal"* ]]; then
@@ -29,4 +41,4 @@ if [[ $TEMP != *"fatal"* ]]; then
 else
     echo "No new tag detected; Latest documentation will be uploaded to opae.github.io"
     /bin/bash ../scripts/push-documentation.sh latest
-fi    
+fi
