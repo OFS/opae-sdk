41d521dd818d5b7cdd7767e5b98acc98356d3aa1
Rodrigo Rojo <rodrigo.rojo@intel.com>
2020-03-24 17:00:38 -0700

    ci: remove object_api from archive

--- scripts/docker_rpm.sh
+++ scripts/docker_rpm.sh
@@ -20,6 +20,7 @@ samples/hello_afu export-ignore
 samples/hello_mpf_afu export-ignore
 samples/intg_xeon_nlb export-ignore
 samples/base export-ignore
+samples/object_api export-ignore
 scripts export-ignore
 tests export-ignore
 tools/fpgametrics export-ignore
