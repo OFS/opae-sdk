fdfd51bb701643a4329b85b523b2714eb3ab0294
Rodrigo Rojo <rodrigo.rojo@intel.com>
2020-04-14 12:53:20 -0700

    ci: remove hello_events from rpm build

--- scripts/docker_rpm.sh
+++ scripts/docker_rpm.sh
@@ -23,6 +23,7 @@ external export-ignore
 platforms export-ignore
 samples/base export-ignore
 samples/hello_afu export-ignore
+samples/hello_events export-ignore
 samples/hello_mpf_afu export-ignore
 samples/intg_xeon_nlb export-ignore
 samples/base export-ignore
