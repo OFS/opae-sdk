96bcabc9e83087e03509d5454113a63e2900ab6b
Ananda Ravuri <33236856+anandaravuri@users.noreply.github.com>
2020-04-16 11:03:56 -0700

    fix code style

--- tools/utilities/vc_image_convert/reverse.py
+++ tools/utilities/vc_image_convert/reverse.py
@@ -11,7 +11,7 @@ LOGGER = logging.getLogger(__name__)
 
 def reverse_bits(x, n):
     result = 0
-    for i in xrange(n):
+    for i in range(n):
         if (x >> i) & 1:
             result |= 1 << (n - 1 - i)
     return result
