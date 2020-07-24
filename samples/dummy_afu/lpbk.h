a0a5bc46003adfbf104b41e13a2a0eab279aa3f0
Rodrigo Rojo <rodrigo.rojo@intel.com>
2020-07-24 13:31:15 -0700

    dummy_afu: fix iova bit shifting

--- samples/dummy_afu/lpbk.h
+++ samples/dummy_afu/lpbk.h
@@ -14,8 +14,8 @@ int run_lpbk(opae::fpga::types::handle::ptr_t h)
   auto source = afu.allocate(64);
   afu.fill(source);
   auto destination = afu.allocate(64);
-  afu.write64(dummy_afu::MEM_TEST_SRC_ADDR, source->io_address() >> 2);
-  afu.write64(dummy_afu::MEM_TEST_DST_ADDR, destination->io_address() >> 2);
+  afu.write64(dummy_afu::MEM_TEST_SRC_ADDR, source->io_address() >> 6);
+  afu.write64(dummy_afu::MEM_TEST_DST_ADDR, destination->io_address() >> 6);
   afu.write64(dummy_afu::MEM_TEST_CTRL, 0x0);
   afu.write64(dummy_afu::MEM_TEST_CTRL, 0b1);
   afu.interrupt_wait(done, 1000);
