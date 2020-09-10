60b2ade3f96afb7e28263c68e06ddef85451170a
Tim Whisonant <tim.whisonant@intel.com>
2020-09-10 12:27:14 -0700

    opae-cxx: fix issue found by checker (#82)
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- include/opae/cxx/core/pvalue.h
+++ include/opae/cxx/core/pvalue.h
@@ -164,7 +164,8 @@ struct pvalue {
   typedef typename std::conditional<std::is_same<T, char *>::value,
                                     typename std::string, T>::type copy_t;
 
-  pvalue() : props_(0), is_set_(false), get_(nullptr), set_(nullptr) {}
+  pvalue()
+      : props_(0), is_set_(false), get_(nullptr), set_(nullptr), copy_() {}
 
   /**
    * @brief pvalue contructor that takes in a reference to fpga_properties
@@ -175,7 +176,7 @@ struct pvalue {
    * @param s The setter function
    */
   pvalue(fpga_properties *p, getter_t g, setter_t s)
-      : props_(p), is_set_(false), get_(g), set_(s) {}
+      : props_(p), is_set_(false), get_(g), set_(s), copy_() {}
 
   /**
    * @brief Overload of `=` operator that calls the wrapped setter
