89b097c26f770182985cc2d3e67a30410728a0fe
Rodrigo Rojo <rodrigo.rojo@intel.com>
2020-10-28 19:16:28 -0700

    doc: update Python req. for sphinx
    * Upgrade the sphinx/requirements.txt to use latest versions of the
      following modules:
      * requests==2.24.0
      * sphinx==3.2.1
      * sphinx-markdown==1.0.2
      * recommonmark==0.6.0
      * breathe==4.23.0
    * Update the sphinx configuration to use recommonmark instead
      of pandoc_markdown.
    * Tweak some of the .rst files according to some warnings emitted by
      sphinx.
      * TODO: Fix other warnings in another pull request.
    * Tweak build-documentation.sh script to simply build the documentation
      but not upload it to github.io
      * TODO: Add CI job to upload to github.io for either master or new
      tags.

--- scripts/build-documentation.sh
+++ scripts/build-documentation.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+set -x
 COMMIT_ID="$(git rev-parse HEAD)"
 TEMP=$((git describe --exact-match $COMMIT_ID) 2>&1)
-export PYTHONPATH=$PWD/lib/python2.7:$PWD/lib/python3.6
+PYTHON_VERSION=$(python3 --version | cut -d ' ' -f2)
 
-if [[ $TEMP == *"fatal"* ]]; then
-    echo "No new tag detected, hence builiding latest docs from master"
-    git reset --hard origin/master
-fi
+#if [[ $TEMP == *"fatal"* ]]; then
+#    echo "No new tag detected, hence builiding latest docs from master"
+#    git reset --hard origin/master
+#fi
 
 #install requirements
-pip install --user -r doc/sphinx/requirements.txt
+python3 -m pip install --user -r doc/sphinx/requirements.txt
 if [ -d mybuild_docs ];
 then
 	rm -rf mybuild_docs
@@ -18,10 +18,12 @@ fi
 
 mkdir mybuild_docs
 pushd mybuild_docs
+export PYTHONPATH=$PWD/lib/python$PYTHON_VERSION
 
 trap "popd" EXIT
 
-cmake .. -DOPAE_BUILD_SPHINX_DOC=ON
+cmake .. -DOPAE_BUILD_SPHINX_DOC=ON -DOPAE_PYTHON_VERSION=$PYTHON_VERSION
+make _opae -j $(nproc)
 make docs
 make manpages
 
@@ -35,10 +37,10 @@ fi
 
 
 
-if [[ $TEMP != *"fatal"* ]]; then
-    echo "New tag detected; Uploading documentation under new tag on opae.github.io"
-    /bin/bash ../scripts/push-documentation.sh new_tag
-else
-    echo "No new tag detected; Latest documentation will be uploaded to opae.github.io"
-    /bin/bash ../scripts/push-documentation.sh latest
-fi
+# if [[ $TEMP != *"fatal"* ]]; then
+#     echo "New tag detected; Uploading documentation under new tag on opae.github.io"
+#     /bin/bash ../scripts/push-documentation.sh new_tag
+# else
+#     echo "No new tag detected; Latest documentation will be uploaded to opae.github.io"
+#     /bin/bash ../scripts/push-documentation.sh latest
+# fi
