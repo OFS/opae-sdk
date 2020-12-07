0a7122b49c5c57f6bd53563f5a78f9f2ccff7855
anandaravuri <ananda.ravuri@intel.com>
2020-12-07 13:35:09 -0800

    add create tarball script
     - Fix code review comments
    
    Signed-off-by: anandaravuri <ananda.ravuri@intel.com>

---
+++ scripts/create-tarball.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+SCRIPT_DIR=`dirname $0`
+SCRIPT_DIR=`realpath $SCRIPT_DIR`
+TOP_DIR=`realpath ${SCRIPT_DIR}/..`
+
+# Input is the name of the directory the tarball will
+# be unpacked to.
+opae_dir=${1:-opae}
+trans="s/opae-sdk/${opae_dir}/"
+
+echo $opae_dir
+
+echo ${TOP_DIR}
+
+cd ${TOP_DIR}/..
+tar --transform=$trans \
+  --exclude=_build \
+  --exclude=.* \
+  --exclude=*~ \
+  --exclude=opae-libs/plugins/ase \
+  --exclude=external/opae-legacy/tests \
+  --exclude=external/opae-legacy/scripts \
+  --exclude=external/opae-legacy/tools/coreidle \
+  --exclude=external/opae-legacy/tools/hssi \
+  --exclude=tools/extra/pyfpgadiag \
+  --exclude=tools/extra/pypackager \
+  -z -c -f ${opae_dir}.tar.gz opae-sdk
+
