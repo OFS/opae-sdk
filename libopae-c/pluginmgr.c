3de6084911fb0da2f1b1e17f2cdaa8b1d328bc9f
Martin Hundebøll <mhu@silicom.dk>
2021-01-21 13:13:57 +0100

    plugins: xfpga: add PCI VID:DID for Silicom Lightning Creek
    Signed-off-by: Martin Hundebøll <mhu@silicom.dk>

--- libopae-c/pluginmgr.c
+++ libopae-c/pluginmgr.c
@@ -58,6 +58,7 @@ typedef struct _platform_data {
 } platform_data;
 
 static platform_data platform_data_table[] = {
+	{ 0x1c2c, 0x1000, "libxfpga.so", 0 },
 	{ 0x8086, 0xbcbd, "libxfpga.so", 0 },
 	{ 0x8086, 0xbcc0, "libxfpga.so", 0 },
 	{ 0x8086, 0xbcc1, "libxfpga.so", 0 },
