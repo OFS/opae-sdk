ee076595605306ea0b69e9d8490946682897a075
Rodrigo Rojo <rodrigo.rojo@intel.com>
2021-02-24 15:28:33 -0800

    rpmbuild: refactored scripts to build/test RPMs (#1887)
    * rpmbuild: refactored scripts to build/test RPMs
    
    * Move docker/buildit.sh to scripts/build-rpms.sh
    * Change build-rpms.sh to take in two arguments:
      * The first is mandatory and points to the cmake build tree root
      * The second is optional and points to the source tree root.
        If omitted, it defaults to the CWD
      * Change how cmake/make is invoked by using command line arguments to
        point to source tree, build tree, or both.
    * Change test-rpms.sh to:
      * Take in path to directory where opae*.rpm should be found.
        If it fails to install the RPMs, the script will error out.
      * Capture exit codes of calling installed
        exectuables. Logging of unexpected exit codes will be shown at the
        end.
    * Change docker files to copy build/test scripts to /scripts directory
    * Change GH actions to call the containers with the proper arguments

--- scripts/test-rpms.sh
+++ scripts/test-rpms.sh
@@ -1,21 +1,50 @@
 #! /bin/bash
 #
-yum install -y *.rpm
-fpgainfo -h
-fpgaconf -h
-pci_device -h
-fpgasupdate -h
-hssi -h
-dummy_afu -h
-opae.io  -h
-PACSign -h
-fpgadiag -h
+rpmdir=$(realpath ${1:-$PWD})
+yum install -y $rpmdir/opae*.rpm
+if [ $? == 1 ]; then
+	echo "Could not install OPAE RPMs"
+	exit 1
+fi
+failures=0
+test_exit_code(){
+	expected_code=$1
+	shift
+	cmd="$@"
+	echo "testing: $cmd"
+	$cmd > output.dat
+	actual_code=$?
+	if [ $expected_code != $actual_code ]; then
+		failures=$(echo 1+$failures | bc)
+		echo "$cmd: expected $expected_code, got $actual_code" >> errors.txt
+		cat output.dat
+	fi
+}
 
-dd if=/dev/urandom  of=dummy.bin bs=1 count=1024
-PACSign PR -H openssl_manager -t UPDATE -y -i dummy.bin -o dummy.signed
+if [ -f errors.txt ]; then
+	unlink errors.txt
+fi
 
+test_exit_code 0 "fpgainfo -h"
+test_exit_code 1 "fpgaconf -h"
+test_exit_code 0 "pci_device -h"
+test_exit_code 0 "fpgasupdate -h"
+test_exit_code 0 "hssi -h"
+test_exit_code 0 "dummy_afu -h"
+test_exit_code 0 "opae.io  -h"
+test_exit_code 0 "PACSign -h"
+test_exit_code 1 "fpgadiag -h"
+test_exit_code 100 "fpgadiag -m lpbk1 -h"
+test_exit_code 100 "fpgadiag -m read -h"
+test_exit_code 100 "fpgadiag -m write -h"
+test_exit_code 100 "fpgadiag -m trput -h"
 
-
-
-
-
+dd if=/dev/urandom  of=dummy.bin bs=1 count=1024 2> /dev/null
+test_exit_code 0 \
+	"PACSign PR -H openssl_manager -t UPDATE -y -i dummy.bin -o dummy.signed"
+echo "unexpected exit codes shown below"
+if [ -f errors.txt ]; then
+	cat errors.txt
+fi
+echo "failures: $failures"
+exit $failures
