2fb632afe8090924b5697aeaddb6f3aa2d85a607
ramyavv <ramyax.venkatesha@intel.com>
2021-05-19 20:17:37 -0700

    Fixing the check fails with build
    Signed-off-by: ramyavv <ramyax.venkatesha@intel.com>

--- afu-test/afu_test.h
+++ afu-test/afu_test.h
@@ -189,6 +189,7 @@ public:
     }
 
     auto tokens = fpga::token::enumerate({filter});
+    perf_token_ = tokens[0];
     if (tokens.size() < 1) {
       if (pci_addr_.empty()) {
         logger_->error("no accelerator found with id: {0}", app_afu_id);
@@ -317,6 +318,7 @@ protected:
   bool shared_;
   uint32_t timeout_msec_;
   fpga::handle::ptr_t handle_;
+  fpga::token::ptr_t perf_token_;
   command::ptr_t current_command_;
   std::map<CLI::App*, command::ptr_t> commands_;
   std::shared_ptr<spdlog::logger> logger_;
