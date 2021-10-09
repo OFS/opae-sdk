952850f430d69ac883e73a9312e959bee1634a61
anandaravuri <ananda.ravuri@intel.com>
2021-10-08 17:01:23 -0700

    tools: add fpgareg tool to dump pcie, hssi, bmc, accelerator registers
    usage: fpgareg.py [-h] [--pcie-address PCIE_ADDRESS]
                      [-r {pcie,bmc,hssi,acc,all}]
    
    optional arguments:
      -h, --help            show this help message and exit
      --pcie-address PCIE_ADDRESS, -P PCIE_ADDRESS
                            sbdf of device to program (e.g. 0000:04:00.0).
                            Optional when one device in system.
      -r {pcie,bmc,hssi,acc,all}, --reg {pcie,bmc,hssi,acc,all}
                            choose reg dump. Default: pcie
    
    - option pcie: memory map pcie bar0, parse DFH and print registers
    - option hssi: find HSSI feature and dump registers
    - option bmc: prints BMC registers
    - option acc: prints accelerator registers
    - option all: prints pice, bmc , hssi ,accelerator registers
    
    Signed-off-by: anandaravuri <ananda.ravuri@intel.com>

--- python/opae.admin/setup.py
+++ python/opae.admin/setup.py
@@ -41,7 +41,7 @@ setup(
             'bitstreaminfo = opae.admin.tools.bitstream_info:main',
             'opaevfio = opae.admin.tools.opaevfio:main',
             'pci_device = opae.admin.tools.pci_device:main',
-            'regmap-debugfs = opae.admin.tools.regmap_debugfs:main',
+            'fpgareg = opae.admin.tools.fpgareg:main',
         ]
     },
     install_requires=[],
