d62f81b3a43b8ce411212f628de9cdc4487c6c60
Tim Whisonant <tim.whisonant@intel.com>
2022-01-06 22:05:31 +0000

    Reverting build-rpms.sh
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- scripts/build-rpms.sh
+++ scripts/build-rpms.sh
@@ -6,26 +6,20 @@ if [ -z ${1+1} ]; then
 fi
 bld=$(realpath $1)
 src=$(realpath ${2:-$PWD})
+cmake=cmake
 
-CMAKE=`which cmake3 2>/dev/null`
-if [ -z $CMAKE ]; then
-  printf "Didn't find cmake3. Looking for cmake.\n"
-  CMAKE=`which cmake 2>/dev/null`
-
-  if [ -z $CMAKE ]; then
-    printf "Didn't find cmake. Aborting..\n"
-    exit 1
-  fi
+if ! command -v cmake > /dev/null && command -v cmake3 > /dev/null; then
+  cmake=cmake3
 fi
 
 mkdir -p $bld
 echo "Building at $bld from source $src"
-$CMAKE -S $src -B $bld -DCPACK_GENERATOR=RPM \
+$cmake -S $src -B $bld -DCPACK_GENERATOR=RPM \
 	               -DOPAE_BUILD_LEGACY=ON \
 	               -DOPAE_PYTHON_VERSION=3.6 \
 	               -DOPAE_BUILD_EXTRA_TOOLS_FPGABIST=ON \
 	               -DCMAKE_INSTALL_PREFIX=/usr
-$CMAKE --build $bld -- package_rpm -j $(nproc)
+$cmake --build $bld -- package_rpm -j $(nproc)
 
 
 
