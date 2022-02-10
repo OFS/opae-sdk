9d28c15a9f90a92b984f051292c6a307b78e8282
Tim Whisonant <tim.whisonant@intel.com>
2022-02-10 10:53:20 -0800

    Update opaevfio.py

--- python/opae.admin/opae/admin/tools/opaevfio.py
+++ python/opae.admin/opae/admin/tools/opaevfio.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright(c) 2020-2021, Intel Corporation
+# Copyright(c) 2020-2022, Intel Corporation
 #
 # Redistribution  and  use  in source  and  binary  forms,  with  or  without
 # modification, are permitted provided that the following conditions are met:
