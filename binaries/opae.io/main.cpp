a845fb74f938bd739f2edaef88e94b3e9c42a9a3
Tim Whisonant <tim.whisonant@intel.com>
2022-09-09 15:24:16 -0700

    Bump opae.admin and opae.io versions.
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- binaries/opae.io/main.cpp
+++ binaries/opae.io/main.cpp
@@ -46,7 +46,7 @@ struct mmio_region *the_region = nullptr;
 const char *program = "opae.io";
 const int major = 0;
 const int minor = 2;
-const int patch = 3;
+const int patch = 4;
 
 py::tuple version()
 {
