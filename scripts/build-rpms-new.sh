34cf3e9daf64cc0e58324df55812b5e72b6ef041
Tim Whisonant <tim.whisonant@intel.com>
2022-12-19 19:30:18 +0000

    again

--- scripts/build-rpms-new.sh
+++ scripts/build-rpms-new.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
 
-[ $# -gt 1 ] && src=$(realpath ${1:-$PWD})
-[ $# -gt 2 ] && src=$(realpath ${2:-$PWD})
+[ $# -eq 1 ] && src=$(realpath ${1:-$PWD})
+[ $# -eq 2 ] && src=$(realpath ${2:-$PWD})
 
 cmake=cmake
 if ! command -v cmake > /dev/null && command -v cmake3 > /dev/null; then
