64d9d9aee62e5eb91a764b6089f3e800176595d0
Tim Whisonant <tim.whisonant@intel.com>
2022-12-29 17:17:03 +0000

    externals: use native cmake support
    * Updates minimum required cmake version to 3.11.
    * Uses cmake-native FetchContent to load external content,
      replacing the opae_external_project_add() home brew.
    * Removes OPAE_PRESERVE_REPOS flag, in favor of
      cmake flag FETCHCONTENT_FULLY_DISCONNECTED.
    * Removes OPAEExternal.cmake
    
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- packaging/opae/rpm/create-tarball.sh
+++ packaging/opae/rpm/create-tarball.sh
@@ -17,23 +17,16 @@ echo ${TOP_DIR}
 cd ${TOP_DIR}/..
 tar --transform=$trans \
   --exclude=__pycache__ \
-  --exclude=_build \
   --exclude=build \
   --exclude=.* \
   --exclude=*~ \
   --exclude=*.so \
   --exclude=doc/sphinx \
-  --exclude=external/gtest \
-  --exclude=external/opae-legacy/tests \
-  --exclude=external/opae-legacy/scripts \
-  --exclude=external/opae-legacy/tools/coreidle \
-  --exclude=external/opae-legacy/tools/hssi \
-  --exclude=external/opae-test \
   --exclude=bin/pyfpgadiag \
   --exclude=bin/pypackager \
   --exclude=packaging/opae/rpm/create-tarball.sh \
-  --exclude=packaging/opae/rpm/create   \
-  --exclude=packaging/opae/rpm/opae.spec   \
-  --exclude=packaging/opae/rpm/clean   \
+  --exclude=packaging/opae/rpm/create \
+  --exclude=packaging/opae/rpm/opae.spec \
+  --exclude=packaging/opae/rpm/clean \
   -z -c -f ${opae_dir}.tar.gz opae-sdk
 
