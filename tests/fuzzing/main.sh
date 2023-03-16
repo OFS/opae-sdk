e7368696c3003a2e4dc2a61e10c68acab33c1924
Tim Whisonant <tim.whisonant@intel.com>
2023-03-16 21:32:42 +0000

    fuzz testing: add mmlink tests
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- tests/fuzzing/main.sh
+++ tests/fuzzing/main.sh
@@ -28,6 +28,7 @@
 shopt -o -s nounset
 
 source fuzz-fpgaconf.sh
+source fuzz-mmlink.sh
 
 fuzz_all() {
   if [ $# -lt 1 ]; then
@@ -37,7 +38,7 @@ fuzz_all() {
   local -i iters=$1
 
   fuzz_fpgaconf ${iters}
-
+  fuzz_mmlink ${iters}
 }
 
 declare -i iters=1
