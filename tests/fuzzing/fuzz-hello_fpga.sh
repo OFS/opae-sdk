41d15bfb9b25890f587d4c19ed84182ff365b269
Tim Whisonant <tim.whisonant@intel.com>
2023-03-17 16:26:38 -0700

    fuzz testing: add hello_fpga tests (#2868)
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

---
+++ tests/fuzzing/fuzz-hello_fpga.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# Copyright(c) 2023, Intel Corporation
+#
+# Redistribution  and  use  in source  and  binary  forms,  with  or  without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of  source code  must retain the  above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name  of Intel Corporation  nor the names of its contributors
+#   may be used to  endorse or promote  products derived  from this  software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,  BUT NOT LIMITED TO,  THE
+# IMPLIED WARRANTIES OF  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT  SHALL THE COPYRIGHT OWNER  OR CONTRIBUTORS BE
+# LIABLE  FOR  ANY  DIRECT,  INDIRECT,  INCIDENTAL,  SPECIAL,  EXEMPLARY,  OR
+# CONSEQUENTIAL  DAMAGES  (INCLUDING,  BUT  NOT LIMITED  TO,  PROCUREMENT  OF
+# SUBSTITUTE GOODS OR SERVICES;  LOSS OF USE,  DATA, OR PROFITS;  OR BUSINESS
+# INTERRUPTION)  HOWEVER CAUSED  AND ON ANY THEORY  OF LIABILITY,  WHETHER IN
+# CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# $ hello_fpga --help
+#
+# hello_fpga
+# OPAE Native Loopback 0 (NLB0) sample
+#
+# Usage:
+#         hello_fpga [-schv] [-S <segment>] [-B <bus>] [-D <device>] [-F <function>] [PCI_ADDR]
+#
+#                 -s,--shared         Open accelerator in shared mode
+#                 -c,--n3000          Assume N3000 MMIO layout
+#                 -h,--help           Print this help
+#                 -v,--version        Print version info and exit
+fuzz_hello_fpga() {
+  if [ $# -lt 1 ]; then
+    printf "usage: fuzz_hello_fpga <ITERS>\n"
+    exit 1
+  fi
+
+  local -i iters=$1
+  local -i i
+  local -i p
+  local -i n
+
+  local -a short_parms=(\
+'-h' \
+'-v' \
+'-S 0x0' \
+'-S 1' \
+'-B 0x0' \
+'-B 2' \
+'-D 0x0' \
+'-D 3' \
+'-F 0x0' \
+'-F 4' \
+'-s' \
+'-c' \
+'0000:00:00.0'\
+)
+
+  local -a long_parms=(\
+'--help' \
+'--version' \
+'--segment 0x0' \
+'--segment 1' \
+'--bus 0x0' \
+'--bus 2' \
+'--device 0x0' \
+'--device 3' \
+'--function 0x0' \
+'--function 4' \
+'--shared' \
+'--n3000' \
+'0000:00:00.0'\
+)
+
+  local cmd=''
+  local -i num_parms
+  local parm=''
+
+  for (( i = 0 ; i < ${iters} ; ++i )); do
+
+    printf "Fuzz Iteration: %d\n" $i
+
+    cmd='hello_fpga '
+    let "num_parms = ${RANDOM} % ${#short_parms[@]}"
+    for (( n = 0 ; n < ${num_parms} ; ++n )); do
+      let "p = ${RANDOM} % ${#short_parms[@]}"
+      parm="${short_parms[$p]}"
+      parm="$(printf %s ${parm} | radamsa)"
+      cmd="${cmd} ${parm}"
+    done
+
+    printf "%s\n" "${cmd}"
+    ${cmd}
+
+    cmd='hello_fpga '
+    let "num_parms = ${RANDOM} % ${#long_parms[@]}"
+    for (( n = 0 ; n < ${num_parms} ; ++n )); do
+      let "p = ${RANDOM} % ${#long_parms[@]}"
+      parm="${long_parms[$p]}"
+      parm="$(printf %s ${parm} | radamsa)"
+      cmd="${cmd} ${parm}"
+    done
+
+    printf "%s\n" "${cmd}"
+    ${cmd}
+
+  done
+}
+
+#declare -i iters=1
+#if [ $# -gt 0 ]; then
+#  iters=$1
+#fi
+#fuzz_hello_fpga ${iters}
