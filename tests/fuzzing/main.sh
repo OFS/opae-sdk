e11e168fc0df9a9194f33f2c4fdd9a5fda134807
Tim Whisonant <tim.whisonant@intel.com>
2023-03-20 10:08:26 -0700

    fuzz testing: add fpgametrics tests (#2870)
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- tests/fuzzing/main.sh
+++ tests/fuzzing/main.sh
@@ -31,6 +31,7 @@ source fuzz-fpgaconf.sh
 source fuzz-fpgad.sh
 source fuzz-hello_fpga.sh
 source fuzz-hello_events.sh
+source fuzz-fpgametrics.sh
 
 fuzz_all() {
   if [ $# -lt 1 ]; then
@@ -43,6 +44,7 @@ fuzz_all() {
   fuzz_fpgad ${iters}
   fuzz_hello_fpga ${iters}
   fuzz_hello_events ${iters}
+  fuzz_fpgametrics ${iters}
 
 }
 
