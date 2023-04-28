e30ecf6e751fe310ec85763c923ae404ce1e5ca8
Tim Whisonant <tim.whisonant@intel.com>
2023-04-28 16:32:41 +0000

    rsu, pci_device: fix rescan path
    When programming the factory image, the number of PFs can be
    reduced. These orphaned PF objects from the prior enumeration can
    cause following setpci calls to fail. Check the output from the
    failed setpci() call before proceeding.
    
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- python/opae.admin/opae/admin/fpga.py
+++ python/opae.admin/opae/admin/fpga.py
@@ -600,7 +600,10 @@ class fpga_base(sysfs_device):
     def clear_device_status(self, device):
         self.log.debug(f'Clearing device status for {device.pci_address}')
         cmd = f'setpci -s {device.pci_address} CAP_EXP+0x08.L'
-        output = int(call_process(cmd), 16)
+        try:
+            output = int(call_process(cmd), 16)
+        except ValueError: # Error during conversion. setpci() call failed.
+            return
         output &= ~0xFF000
         output |= 0xF5000
         call_process(f'{cmd}={output:08x}')
