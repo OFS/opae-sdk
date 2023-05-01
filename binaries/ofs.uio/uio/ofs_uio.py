a84c6ab38e02eb29e9e3343ad3872653efb7ddac
anandaravuri <ananda.ravuri@intel.com>
2023-05-01 14:18:36 -0700

    fix code reivew comments
    Signed-off-by: anandaravuri <ananda.ravuri@intel.com>

--- binaries/ofs.uio/uio/ofs_uio.py
+++ binaries/ofs.uio/uio/ofs_uio.py
@@ -63,6 +63,8 @@ MAILBOX_POLL_SLEEP_TIME = 1/1000000
 # mailbox register poll timeout 100 microseconds
 MAILBOX_POLL_TIMEOUT = 1/10000
 
+MBOX_JSON_VAL = re.compile("0[xX][0-9a-fA-F]+", re.IGNORECASE)
+
 
 def verify_pcie_address(pcie_address):
     m = BDF_PATTERN.match(pcie_address)
@@ -72,10 +74,9 @@ def verify_pcie_address(pcie_address):
     return True
 
 
-def verify_hex(str):
-    regx = re.compile("0[xX][0-9a-fA-F]+")
+def verify_hex(string):
     try:
-        if re.fullmatch(regx, str) is not None:
+        if re.fullmatch(MBOX_JSON_VAL, string) is not None:
             return True
         else:
             return False
@@ -83,35 +84,31 @@ def verify_hex(str):
         return False
 
 
-def verify_json(str):
-    if os.path.isfile(str) is not True:
-        raise argparse.ArgumentTypeError('Input file does not exist:', str)
-    if pathlib.Path(str).suffix != ".json":
-         raise argparse.ArgumentTypeError('Input file is not json file:', str)
+def verify_json(file_json):
+    if os.path.isfile(file_json) is not True:
+        raise argparse.ArgumentTypeError('Input file does not exist:', file_json)
+    if pathlib.Path(file_json).suffix != ".json":
+        raise argparse.ArgumentTypeError('Input file is not json file:', file_json)
 
-
-    with open(str, 'r') as f:
+    with open(file_json, 'r') as f:
         try:
             data = json.load(f)
-            print(data)
         except ValueError:
             print('Decoding JSON has failed')
-            raise argparse.ArgumentTypeError('Decoding JSON has failed', str)
+            raise argparse.ArgumentTypeError('Decoding JSON has failed', file_json)
 
-        for i in data:
-            print(i)
         for i in data:
             if i.get('address') is not None:
                 if verify_hex(i.get('address')) is False:
                     raise argparse.ArgumentTypeError('{} file contains invalid hex address {} {}'
-                  . format(str, i.get('reg-name'),i.get('address') ))
+                    .format(file_json, i.get('reg-name'), i.get('address')))
 
             if i.get('value') is not None:
                 if verify_hex(i.get('value')) is False:
                     print('{} file contains invalid hex value {} {}'
-                  . format(str, i.get('reg-name'),i.get('value') ) )
-                    raise argparse.ArgumentTypeError('Input file is not json file:', str)
-    return str
+                    .format(file_json, i.get('reg-name'), i.get('value')))
+                    raise argparse.ArgumentTypeError('Input file is not json file:', file_json)
+    return data
 
 
 class verify_input_hex(argparse.Action):
@@ -140,15 +137,15 @@ class verify_input_hex(argparse.Action):
             setattr(namespace, self.dest, values)
 
 
-def verify_uio(str):
+def verify_uio(string):
     regx = re.compile("uio[0-9]+")
     try:
-        if re.fullmatch(regx, str) is not None:
-            return str
+        if re.fullmatch(regx, string) is not None:
+            return string
         else:
-            raise argparse.ArgumentTypeError('Invalid input:', str)
+            raise argparse.ArgumentTypeError('Invalid input:', string)
     except ValueError:
-        raise argparse.ArgumentTypeError('Invalid input:', str)
+        raise argparse.ArgumentTypeError('Invalid input:', string)
 
 
 class FpgaFinder(object):
@@ -627,11 +624,10 @@ def parse_args():
                         default=None, nargs=2, metavar=('address', 'value'),
                         help=mailbox_write_help)
 
-
     mailbox_json_help = 'Write Mailbox json file address Values \
                         (e.g.--mailbox-json msi-x.json).'
-    parser.add_argument('--mailbox-json', 
-                        default=None,type=verify_json,
+    parser.add_argument('--mailbox-json',
+                        default=None, type=verify_json,
                         help=mailbox_json_help)
 
     return parser, parser.parse_args()
@@ -738,24 +734,14 @@ def main():
                           .format(hex(addr), hex(value)))
 
         elif args.mailbox_json is not None:
-            with open(args.mailbox_json, 'r') as f:
-                try:
-                    data = json.load(f)
-                except ValueError:
-                    print('Decoding JSON has failed')
-
-                for i in data:
-                    print('reg-name:{} address:{} value:{}'.format(i.get('reg-name'),
-                                                                   i.get('address'),
-                                                                   i.get('value')))
-                    if not uio.mailbox_write(args.region_index, int(i.get('address'), 16),
-                                             int(i.get('value'), 16)):
-                        print('Failed to write Mailbox CSR {} address:{}'
-                              .format(i.get('reg-name'),i.get('address')))
-                    else:
-                        print('MailboxWrite({}:{}):{}'
-                              .format(i.get('reg-name'),i.get('address'), i.get('value')))
-
+            for i in args.mailbox_json:
+                if not uio.mailbox_write(args.region_index, int(i.get('address'), 16),
+                                         int(i.get('value'), 16)):
+                    print('Failed to write Mailbox CSR {} address:{}'
+                    .format(i.get('reg-name'), i.get('address')))
+                else:
+                    print('MailboxWrite({}:{}):{}'
+                    .format(i.get('reg-name'), i.get('address'), i.get('value')))
 
     except Exception as e:
         print(e)
