9637c6b59179a2216810cd21d91ac1f8a44883a0
Tim Whisonant <tim.whisonant@intel.com>
2023-12-11 09:54:59 -0800

    Feature: add opae-mem (#3055)
    ### Description
    Adding opae-mem tool and updates to the OPAE Python bindings that support it.
    opae-mem is intended to be a replacement for ofs.uio.
    
    
    ### Collateral (docs, reports, design examples, case IDs):
    
    
    - [x] Document Update Required? (Specify FIM/AFU/Scripts)
    
    ### Tests added:
    
    
    ### Tests run:
    CI
    
    Signed-off-by: Tim Whisonant <tim.whisonant@intel.com>

--- libraries/plugins/uio/dfl.c
+++ libraries/plugins/uio/dfl.c
@@ -29,6 +29,7 @@
 #endif // HAVE_CONFIG_H
 
 #include <time.h>
+#include <sys/pci.h>
 
 #include "opae_int.h"
 #include "opae_uio.h"
@@ -180,6 +181,12 @@ int walk_fme(uio_pci_device_t *dev, struct opae_uio *u,
 			continue;
 
 		bar = port_offset_reg.bits.bar;
+		if (bar >= PCI_STD_NUM_BARS) {
+			OPAE_DBG("ignoring invalid BAR %d at offset 0x%x",
+				 bar, fme_ports[i]);
+			continue;
+		}
+
 		if (opae_uio_region_get(u, bar, &port_mmio, &size)) {
 			OPAE_ERR("failed to get Port BAR %d", bar);
 			continue;
