OFS/opae-sdk

commit c97c3f2863f0e34f266900b51c6ea86865160562

pci_device: Add bridge reset when a device is unplugged (#3143)
Signed-off-by: Michael Adler <michael.adler@intel.com>

python/opae.admin/opae/admin/sysfs.py
@@ -28,6 +28,7 @@ from __future__ import absolute_import
 import glob
 import os
 import re
+import time
 from contextlib import contextmanager
 from pathlib import Path
 from subprocess import CalledProcessError, check_call, DEVNULL
@@ -228,6 +229,8 @@ class pci_node(sysfs_node):
         self._aer_cmd1 = 'setpci -s {} ECAP_AER+0x08.L'.format(
             pci_address['pci_address'])
         self._aer_cmd2 = 'setpci -s {} ECAP_AER+0x14.L'.format(
+            pci_address['pci_address'])
+        self._bridge_ctrl_cmd = 'setpci -s {} BRIDGE_CONTROL'.format(
             pci_address['pci_address'])
         if self.have_node('driver'):
             self._driver = os.readlink(self.node('driver').sysfs_path)
@@ -595,6 +598,26 @@ class pci_node(sysfs_node):
             call_process('{}={:#08x}'.format(self._aer_cmd2, values[1]))
         except CalledProcessError as err:
             self.log.warn('error setting aer: %s', err)
+
+    def reset_bridge(self):
+        """reset_bridge Reset the devices under root port bridge object.
+
+        Notes:
+            This relies on calling 'setpci' and will log an exception if an
+            error was encountered while calling 'setpci'.
+        """
+        try:
+            # Get current bridge control value
+            bc = int(call_process(self._bridge_ctrl_cmd), 16)
+            # Enable bus reset
+            bc_reset = bc | 0x40
+            call_process(f"{self._bridge_ctrl_cmd}=0x{bc_reset:x}")
+            time.sleep(0.1)
+            # Clear bus reset
+            call_process(f"{self._bridge_ctrl_cmd}=0x{bc:x}")
+            time.sleep(0.25)
+        except CalledProcessError as err:
+            self.log.warn('error resetting bridge: %s', err)
 
     @property
     def sriov_totalvfs(self):

python/opae.admin/opae/admin/tools/pci_device.py
@@ -24,6 +24,7 @@
 # CONTRACT,  STRICT LIABILITY,  OR TORT  (INCLUDING NEGLIGENCE  OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+import logging
 import re
 import sys
 import subprocess
@@ -260,6 +261,10 @@ class unplug(object):
 
         if remove:
             if debug:
+                print(f'Resetting the root bridge {root.pci_address}')
+            root.reset_bridge()
+
+            if debug:
                 print(f'Removing the root device {root.pci_address}')
             root.remove()
 
@@ -348,6 +353,11 @@ def main():
                'plug': (plug.add_subparser, plug())}
 
     parser = ArgumentParser()
+
+    log_levels = ['verbose', 'info']
+    parser.add_argument('--log-level', choices=log_levels,
+                        default='info', help='log level to use')
+
     parser.add_argument('devices', type=pci_devices,
                         metavar='device-filter',
                         help=('PCIe filter of device '
@@ -365,6 +375,11 @@ def main():
         actions[k][0](subparser)
 
     args = parser.parse_args()
+
+    if args.log_level == 'info':
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+    else:
+        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
     if hasattr(args, 'which') and args.which and args.which == 'plug':
         # Force a PCI bus rescan.

python/opae.admin/opae/admin/utils/process.py
@@ -36,6 +36,7 @@ def call_process(cmd, no_dry=False):
         print(cmd)
         return '0x0' if 'setpci' in cmd else ''
 
+    LOG('process').error('Calling %s', cmd)
     try:
         output = subprocess.check_output(cmd.split(),
                                          stderr=subprocess.STDOUT)

commit a518a133c3859bc9da8fbb9cc75a96d07c3baf87

pci_device: improved reset sequence
- Wait for data link active after triggering a bridge's secondary device reset.
- Move reset to a separate command that unloads drivers, then devices, triggers
  warm reset on FPGAs, and finally rescans and clears errors.

Fixes: c97c3f2863f0 ("pci_device: Add bridge reset when a device is unplugged (#3143)")

Signed-off-by: Michael Adler <michael.adler@intel.com>

python/opae.admin/opae/admin/sysfs.py
@@ -232,6 +232,8 @@ class pci_node(sysfs_node):
             pci_address['pci_address'])
         self._bridge_ctrl_cmd = 'setpci -s {} BRIDGE_CONTROL'.format(
             pci_address['pci_address'])
+        self._link_status_cmd = 'setpci -s {} CAP_EXP+12.W'.format(
+            pci_address['pci_address'])
         if self.have_node('driver'):
             self._driver = os.readlink(self.node('driver').sysfs_path)
 
@@ -599,8 +601,39 @@ class pci_node(sysfs_node):
         except CalledProcessError as err:
             self.log.warn('error setting aer: %s', err)
 
+    def data_link_layer_active(self):
+        """data_link_layer_active Return true if PCIe data link is active.
+
+        Returns:
+            Bit 13 of the PCIe link status register.
+
+        Notes:
+            This relies on calling 'setpci' and will log an exception if an
+            error was encountered while calling 'setpci'.
+        """
+        try:
+            ls = int(call_process(self._link_status_cmd), 16)
+            return (ls >> 13) & 1
+        except CalledProcessError as err:
+            self.log.warn('error reading link status: %s', err)
+
+    def wait_for_data_link(self, target_state):
+        """wait_for_data_link Wait until data link active state matches
+                              target_state.
+        """
+        # Bridges are not required to implement the link layer active bit.
+        # Time out after a generous 5 seconds, assuming the target state
+        # is reached.
+        for i in range(5):
+            if self.data_link_layer_active() == target_state:
+                break
+            time.sleep(1)
+
+        # The spec requires at least 100ms delay after the link change.
+        time.sleep(0.25)
+
     def reset_bridge(self):
-        """reset_bridge Reset the devices under root port bridge object.
+        """reset_bridge Reset secondary devices under root port bridge object.
 
         Notes:
             This relies on calling 'setpci' and will log an exception if an
@@ -609,13 +642,15 @@ class pci_node(sysfs_node):
         try:
             # Get current bridge control value
             bc = int(call_process(self._bridge_ctrl_cmd), 16)
-            # Enable bus reset
+
+            # Enable secondary bus reset
             bc_reset = bc | 0x40
             call_process(f"{self._bridge_ctrl_cmd}=0x{bc_reset:x}")
-            time.sleep(0.1)
+            self.wait_for_data_link(0)
+
             # Clear bus reset
             call_process(f"{self._bridge_ctrl_cmd}=0x{bc:x}")
-            time.sleep(0.25)
+            self.wait_for_data_link(1)
         except CalledProcessError as err:
             self.log.warn('error resetting bridge: %s', err)
 

python/opae.admin/opae/admin/tools/pci_device.py
@@ -261,10 +261,6 @@ class unplug(object):
 
         if remove:
             if debug:
-                print(f'Resetting the root bridge {root.pci_address}')
-            root.reset_bridge()
-
-            if debug:
                 print(f'Removing the root device {root.pci_address}')
             root.remove()
 
@@ -314,6 +310,54 @@ class plug(object):
         call_process(f'{cmd}=FFFFFFFF')
 
 
+class reset(object):
+    @staticmethod
+    def add_subparser(subparser):
+        reset = subparser.add_parser('reset')
+        reset.add_argument('-d', '--debug', action='store_true',
+                           default=False, help='enable debug output')
+
+    def __call__(self, device, args):
+        debug = args.debug
+
+        root = device.pci_node.root
+
+        v0, v1 = None, None
+        if root.supports_ecap('aer'):
+            if debug:
+                print('ECAP_AER is supported')
+            v0, v1 = root.aer
+            root.aer = (0xFFFFFFFF, 0xFFFFFFFF)
+
+        reset.reset_node(root, debug)
+
+        if v0:
+            root.aer = (v0, v1)
+
+    @staticmethod
+    def reset_node(root, debug):
+        if debug:
+            print('Unbinding drivers for leaf devices')
+
+        for e in root.endpoints:
+            if debug:
+                print(f' unbind {e.pci_address}')
+            e.unbind()
+
+        for e in root.endpoints:
+            if debug:
+                print(f' remove {e.pci_address}')
+            try:
+                e.remove()
+            except NameError:
+                None # Ignore endpoints with no sysfs remove entry
+
+        if debug:
+            print(f'Resetting secondary devices under {root.pci_address}')
+        root.reset_bridge()
+        root.rescan()
+
+
 def add_unbind_subparser(subparser):
     subparser.add_parser('unbind')
 
@@ -350,7 +394,8 @@ def main():
                'aer': (aer.add_subparser, aer()),
                'topology': (add_topology_subparser, topology),
                'unplug': (unplug.add_subparser, unplug()),
-               'plug': (plug.add_subparser, plug())}
+               'plug': (plug.add_subparser, plug()),
+               'reset': (reset.add_subparser, reset())}
 
     parser = ArgumentParser()
 
@@ -397,6 +442,9 @@ def main():
                 actions['vf'][1](dev, args, args.numvfs)
             else:
                 actions[args.which][1](dev, args)
+                # Clear AER after reset
+                if args.which == 'reset':
+                    actions['plug'][1](dev, args)
         else:
             actions['topology'][1](dev, args)
 